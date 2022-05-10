1c5e5230143d29fe4c51a23114e3477ddb4658d1
MildogMiller <37893674+MildogMiller@users.noreply.github.com>
2022-05-10 12:31:18 +0200

    ✨ Added Integration to google maps

--- master/libs/client/demo/src/lib/demo-map-page/demo-map-page.component.ts
+++ master/libs/client/demo/src/lib/demo-map-page/demo-map-page.component.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @angular-eslint/no-empty-lifecycle-method */
+/* eslint-disable @typescript-eslint/no-empty-function */
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,6 +8,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./demo-map-page.component.scss'],
 })
 export class DemoMapPageComponent implements OnInit {
+  Reserve:any= {
+    code : 200,
+    status : "success",
+    explanation : "",
+    data : {
+        reserveName : "UP",
+        center : {
+            latitude : "-25.755123",
+            longitude : "28.231999"
+        },
+        location : [
+            {
+                latitude : "-25.753785",
+                longitude : "28.231703"
+            },
+            {
+                latitude : "-25.755650",
+                longitude : "28.230737"
+            },
+            {
+                latitude : "-25.757089",
+                longitude : "28.233456"
+            },
+            {
+                latitude : "-25.756385",
+                longitude : "28.236474"
+            },
+            {
+                latitude : "-25.754765",
+                longitude : "28.235663"
+            }
+        ]
+    }
+}
   constructor() {}
 
   ngOnInit(): void {}
--- master/libs/client/maps/src/lib/interactive-map/interactive-map.component.ts
+++ master/libs/client/maps/src/lib/interactive-map/interactive-map.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+/* eslint-disable @angular-eslint/no-empty-lifecycle-method */
+/* eslint-disable @typescript-eslint/no-empty-function */
+import { Component, Input, OnInit } from '@angular/core';
+import { catchError, map, Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { ignoreElements } from 'rxjs';
 
 @Component({
   selector: 'master-interactive-map',
@@ -6,7 +11,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./interactive-map.component.scss'],
 })
 export class InteractiveMapComponent implements OnInit {
-  constructor() {}
+  @Input() Reserve: any = null;
+  @Input() MapOutline: Array<google.maps.LatLng> | null = null;
+  apiready: Observable<boolean>;
+  center: google.maps.LatLngLiteral = {
+    lat: -25.739803433797874,
+    lng: 27.91649993973261,
+  };
+  zoom = 15;
 
-  ngOnInit(): void {}
+  constructor(private http: HttpClient) {
+    //checks if api is loaded and working correctly
+    this.apiready = http
+      .jsonp(
+        'https://maps.googleapis.com/maps/api/js?key=AIzaSyAAloEcLyO1ZY1dRgopJx63GX-Bvh3P7fE',
+        'callback'
+      )
+      .pipe(
+        map(() => {
+          //set map view
+          if (this.Reserve == null) {
+            //show error message of no input
+          } else if (this.Reserve['code'] != 200) {
+            //show error message of bad response code
+          } else {
+            this.center = {
+              lat: parseFloat(this.Reserve['data']['center']['latitude']),
+              lng: parseFloat(this.Reserve['data']['center']['longitude']),
+            };
+            if (this.Reserve['data']['location'].length > 2) {
+              this.MapOutline = this.Reserve['data']['location'].map(
+                (x: any) => ({
+                  lat: parseFloat(x.latitude),
+                  lng: parseFloat(x.longitude),
+                })
+              );
+            }
+          }
+          return true;
+        }),
+        catchError(() => of(false))
+      );
+  }
+
+  ngOnInit(): void {
+  }
 }
