a86b3fe2e87143b191aeb49a7b1119f78bff1851
t3ddy33 <u20493836@tuks.co.za>
2022-05-11 20:16:20 +0200

    ✅ Corrected files which returned erroneous results

--- master/libs/client/map-apicaller/src/lib/map-caller/map-caller.service.spec.ts
+++ master/libs/client/map-apicaller/src/lib/map-caller/map-caller.service.spec.ts
@@ -1,12 +1,12 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { MapCallerService } from './map-caller.service';
 
 describe('MapCallerService', () => {
   let service: MapCallerService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({ imports: [HttpClientTestingModule] });
     service = TestBed.inject(MapCallerService);
   });
 
