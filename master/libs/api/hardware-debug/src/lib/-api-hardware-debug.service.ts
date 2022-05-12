dc67ef8c647bcbd41c370c3d9dfc5feac23753f4
Isakvdw <u20468203@tuks.co.za>
2022-05-12 11:58:26 +0200

    🚧 Update hardware-API controller format

--- master/libs/api/hardware-debug/src/lib/-api-hardware-debug.service.ts
+++ master/libs/api/hardware-debug/src/lib/-api-hardware-debug.service.ts
@@ -3,67 +3,66 @@ import { acknowledge } from './hardware-payload.interface';
 
 @Injectable()
 export class ApiHardwareDebugService {
+  deviceStatusProcess(content: any): acknowledge {
+    console.log(content);
+    return {
+      code: 200,
+      status: 'ACK',
+      explanation: 'Data received...',
+    };
+  }
 
-    deviceStatusProcess(content : any) : acknowledge {
-        console.log(content);
-        return {
-            code : 200,
-            status : "ACK",
-            explanation : "Data received..."
-        }
+    deviceUpProcess(content: any): acknowledge {
+    console.log(content);
+    return {
+        code: 200,
+        status: 'ACK',
+        explanation: 'Data received...',
+    };
     }
 
-    deviceUpProcess(content : any) : acknowledge {
-        console.log(content);
-        return {
-            code : 200,
-            status : "ACK",
-            explanation : "Data received..."
-        }
-    }
+  deviceJoinProcess(content: any): acknowledge {
+    console.log(content);
+    return {
+      code: 200,
+      status: 'ACK',
+      explanation: 'Data received...',
+    };
+  }
 
-    deviceJoinrocess(content : any) : acknowledge {
-        console.log(content);
-        return {
-            code : 200,
-            status : "ACK",
-            explanation : "Data received..."
-        }
-    }
+  deviceAckProcess(content: any): acknowledge {
+    console.log(content);
+    return {
+      code: 200,
+      status: 'ACK',
+      explanation: 'Data received...',
+    };
+  }
 
-    deviceAckProcess(content : any) : acknowledge {
-        console.log(content);
-        return {
-            code : 200,
-            status : "ACK",
-            explanation : "Data received..."
-        }
-    }
+  deviceErrorProcess(content: any): acknowledge {
+    console.log(content);
+    return {
+      code: 200,
+      status: 'ACK',
+      explanation: 'Data received...',
+    };
+  }
 
-    deviceErrorProcess(content : any) : acknowledge {
-        console.log(content);
-        return {
-            code : 200,
-            status : "ACK",
-            explanation : "Data received..."
-        }
-    }
+  deviceLocationProcess(content: any): acknowledge {
+    console.log(content);
+    return {
+      code: 200,
+      status: 'ACK',
+      explanation: 'Data received...',
+    };
+  }
 
-    deviceLocationProcess(content : any) : acknowledge {
-        console.log(content);
-        return {
-            code : 200,
-            status : "ACK",
-            explanation : "Data received..."
-        }
-    }
-
-    deviceTxackProcess(content : any) : acknowledge {
-        console.log(content);
-        return {
-            code : 200,
-            status : "ACK",
-            explanation : "Data received..."
-        }
-    }
+  deviceTxackProcess(content: any): acknowledge {
+    console.log(content);
+    return {
+      code: 200,
+      status: 'ACK',
+      explanation: 'Data received...',
+    };
+  }
 }
