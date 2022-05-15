f026bb0705d5a2b62500be237fc059a4de8aae23
MildogMiller <37893674+MildogMiller@users.noreply.github.com>
2022-05-15 11:20:03 +0200

    🔥 Removed Google maps library

--- master/apps/lora-tracking/src/app/app-routing.module.ts
+++ master/apps/lora-tracking/src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { Routes,RouterModule } from '@angular/router';
 import {HomescreenComponent} from '@master/client/defaultpage'
 import {DemoMapPageComponent} from '@master/client/demo'
-import { InteractiveMapComponent } from '@master/client/maps';
 import { ReserveMapComponent } from '@master/client/leaflet-library';
 const routes: Routes = [
   {path:"",component:DemoMapPageComponent},
