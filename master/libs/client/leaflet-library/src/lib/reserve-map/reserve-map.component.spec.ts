42a316975c69772d0b8f668edc28353e51f75454
MildogMiller <37893674+MildogMiller@users.noreply.github.com>
2022-05-15 10:39:33 +0200

    ✨ added leaflet component

---
+++ master/libs/client/leaflet-library/src/lib/reserve-map/reserve-map.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ReserveMapComponent } from './reserve-map.component';
+
+describe('ReserveMapComponent', () => {
+  let component: ReserveMapComponent;
+  let fixture: ComponentFixture<ReserveMapComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ReserveMapComponent],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ReserveMapComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ master/libs/client/leaflet-library/src/lib/reserve-map/reserve-map.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'reserve-map',
+  templateUrl: './reserve-map.component.html',
+  styleUrls: ['./reserve-map.component.scss'],
+})
+export class ReserveMapComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
