f681c2e1a9ad9590ceeb1cb2782197f9ac394aca
MildogMiller <37893674+MildogMiller@users.noreply.github.com>
2022-05-16 10:05:20 +0200

    ✨ Added Ability to dynamically change up

--- master/libs/shared-interfaces/src/index.ts
+++ master/libs/shared-interfaces/src/index.ts
@@ -1,2 +1,3 @@
 export * from './lib/shared-interfaces.module';
 export * from './lib/map-interfaces'
+export * from './lib/map-options-interfaces'
---
+++ master/libs/shared-interfaces/src/lib/map-options-interfaces.ts
@@ -0,0 +1,7 @@
+export enum ViewMapType{
+    NORMAL_OPEN_STREET_VIEW,SATELLITE_ESRI_1
+}
+
+export enum MapRender{
+    ALL,WITH_POLYGON
+}
