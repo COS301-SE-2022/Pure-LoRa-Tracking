8364a4ceadad4bde37f5c804fb2495d0c7c96f67
LiamBurgess18015001 <u18015001@tuks.co.za>
2022-05-23 08:49:43 +0200

    ✨ (database): mongoose connection

--- master/apps/api/src/app/app.module.ts
+++ master/apps/api/src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { ApiApiRouterModule } from '@lora/apiRouter'
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ApiApiRouterModule,ConfigModule.forRoot()],
+  imports: [ApiApiRouterModule,ConfigModule.forRoot(),
+    MongooseModule.forRoot('mongodb://localhost/nest')],
 })
 export class AppModule {}
