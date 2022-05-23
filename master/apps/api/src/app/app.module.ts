ae70fd1ec3f500c19a458d2f190236370d316a51
LiamBurgess18015001 <u18015001@tuks.co.za>
2022-05-23 09:38:51 +0200

    ✨ (database): basic interface

--- master/apps/api/src/app/app.module.ts
+++ master/apps/api/src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { ApiApiRouterModule } from '@lora/apiRouter'
-import { MongooseModule } from '@nestjs/mongoose';
+import { DatabaseProxyModule } from '@lora/database'
 
 @Module({
   imports: [ApiApiRouterModule,ConfigModule.forRoot(),
-    MongooseModule.forRoot('mongodb://localhost/nest')],
+    DatabaseProxyModule
+    ],
 })
 export class AppModule {}
--- master/libs/database-proxy/src/lib/database-proxy.module.ts
+++ master/libs/database-proxy/src/lib/database-proxy.module.ts
@@ -1,8 +1,13 @@
 import { Module, Global } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { DatabaseProxyService } from './database-proxy.service';
+import { rssiData, rssiDataSchema } from '../database-interfaces.interface';
 
 @Global()
 @Module({
+  imports : [MongooseModule.forRoot('mongodb://localhost/lora'),
+              MongooseModule.forFeature([{name:rssiData.name, schema:rssiDataSchema}])
+],
   controllers: [],
   providers: [DatabaseProxyService],
   exports: [DatabaseProxyService],
