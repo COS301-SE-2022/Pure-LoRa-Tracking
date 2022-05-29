d7a658f224b27c23057c9b1f9ad90e2fcfac55d5
Isakvdw <u20468203@tuks.co.za>
2022-05-29 21:24:52 +0200

    ✅ list devices test

--- master/libs/chirpstack/chirpstack-sensor/src/lib/chirpstack-chirpstack-sensor.service.spec.ts
+++ master/libs/chirpstack/chirpstack-sensor/src/lib/chirpstack-chirpstack-sensor.service.spec.ts
@@ -1,5 +1,9 @@
 import { Test } from '@nestjs/testing';
 import { ChirpstackChirpstackSensorService } from './chirpstack-chirpstack-sensor.service';
+// import {Metadata} from '@grpc/grpc-js/src/metadata';
+// import {Metadata} from '@grpc/grpc-js';
+// import { DeviceServiceClient } from '@chirpstack/chirpstack-api/as/external/api/device_grpc_pb';
+
 
 describe('ChirpstackChirpstackSensorService', () => {
   let service: ChirpstackChirpstackSensorService;
@@ -7,12 +11,25 @@ describe('ChirpstackChirpstackSensorService', () => {
   beforeEach(async () => {
     const module = await Test.createTestingModule({
       providers: [ChirpstackChirpstackSensorService],
+      imports: [],
     }).compile();
 
+    await module.init()
     service = module.get(ChirpstackChirpstackSensorService);
   });
 
   it('should be defined', () => {
     expect(service).toBeTruthy();
   });
+
+  test('it should run', done => {
+    const getAttributeMock = jest.fn((error, data) => {
+      console.log(data);
+      done();
+    });
+    const authtoken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlfa2V5X2lkIjoiMWIwMmZhNDAtMzI4OS00NzllLWI2NjUtM2MwMzg4YmEzZDRmIiwiYXVkIjoiYXMiLCJpc3MiOiJhcyIsIm5iZiI6MTY1Mzg1MDYwNywic3ViIjoiYXBpX2tleSJ9.epxodFKkLwrvinNBVgo0r9k4PWLxumzAGw61oKrTMrI';
+    service.list_devices(getAttributeMock, authtoken);
+    // expect(getAttributeMock).toHaveBeenCalled();
+  });
+  
 });
