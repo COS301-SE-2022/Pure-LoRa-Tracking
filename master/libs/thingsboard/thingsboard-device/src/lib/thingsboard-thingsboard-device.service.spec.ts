ecd22a228838909b663e6d9e37ea4adc5e89d9fb
t3ddy33 <u20493836@tuks.co.za>
2022-05-30 02:00:42 +0200

    🔨  Updated assign device test.

--- master/libs/thingsboard/thingsboard-device/src/lib/thingsboard-thingsboard-device.service.spec.ts
+++ master/libs/thingsboard/thingsboard-device/src/lib/thingsboard-thingsboard-device.service.spec.ts
@@ -49,9 +49,11 @@ describe('ThingsboardThingsboardDeviceService', () => {
   it('should assign the specified device to the specified customer and return status 200', async () => {
     /*const data = await loginService.login(username, password);
     service.setToken(data['data']['token']);
+    const userinfo = await loginService.userInfo(data['data']['token']);
     const custID = userinfo['data']['customerId']['id'];
+    console.log(custID);
     expect(custID).toBeDefined();
-    expect(await service.deleteDevice("8e4fcc90-dc0e-11ec-931b-3544ea43758e")).toEqual(true);*/
+    expect(await service.assignDevicetoCustomer("9fed2a30-dfa9-11ec-b99c-f7477a3db362", "7beb3910-df9f-11ec-b124-d5b7f09de50f")).toEqual(true);*/
   })
 
   it('should unassign the specified device from the specified customer and return status 200', async () => {
@@ -59,6 +61,6 @@ describe('ThingsboardThingsboardDeviceService', () => {
     service.setToken(data['data']['token']);
     const custID = userinfo['data']['customerId']['id'];
     expect(custID).toBeDefined();
-    expect(await service.deleteDevice("8e4fcc90-dc0e-11ec-931b-3544ea43758e")).toEqual(true);*/
+    expect(await service.deleteDevice("7beb3910-df9f-11ec-b124-d5b7f09de50f")).toEqual(true);*/
   })
 });
