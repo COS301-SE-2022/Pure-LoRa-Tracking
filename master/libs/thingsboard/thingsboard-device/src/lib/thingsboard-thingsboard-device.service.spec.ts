b52e3267effe85bc144be95bc31b05a352397274
t3ddy33 <u20493836@tuks.co.za>
2022-06-03 21:35:41 +0200

    ✅ (testing) Created assign device test mock data.

--- master/libs/thingsboard/thingsboard-device/src/lib/thingsboard-thingsboard-device.service.spec.ts
+++ master/libs/thingsboard/thingsboard-device/src/lib/thingsboard-thingsboard-device.service.spec.ts
@@ -1,14 +1,17 @@
 import { ThingsboardThingsboardUserModule, ThingsboardThingsboardUserService } from '@lora/thingsboard-user';
-import { HttpModule } from '@nestjs/axios';
+import { HttpModule, HttpService } from '@nestjs/axios';
 import { Test } from '@nestjs/testing';
 import { ThingsboardThingsboardDeviceService } from './thingsboard-thingsboard-device.service';
-
+import { AxiosResponse } from 'axios';
+import { of } from 'rxjs';
 
 describe('ThingsboardThingsboardDeviceService', () => {
   let service: ThingsboardThingsboardDeviceService;
   let loginService: ThingsboardThingsboardUserService;
+  let httpService: HttpService;
   const username = "reserveAdmin@reserve.com";
   const password = "reserve";
+  
 
   beforeEach(async () => {
     const module = await Test.createTestingModule({
@@ -18,6 +21,7 @@ describe('ThingsboardThingsboardDeviceService', () => {
 
     service = module.get(ThingsboardThingsboardDeviceService);
     loginService = module.get(ThingsboardThingsboardUserService);
+    httpService = module.get(HttpService);
   });
 
   it('should be defined', () => {
@@ -47,6 +51,52 @@ describe('ThingsboardThingsboardDeviceService', () => {
   })
 
   it('should assign the specified device to the specified customer and return status 200', async () => {
+    const result: AxiosResponse<any> = {
+      data: {
+        id: {
+          id: "784f394c-42b6-435a-983c-b7beff2784f9",
+          entityType: "DEVICE"
+        },
+        createdTime: 1609459200000,
+        tenantId: {
+          id: "784f394c-42b6-435a-983c-b7beff2784f9",
+          entityType: "TENANT"
+        },
+        customerId: {
+          id: "784f394c-42b6-435a-983c-b7beff2784f9",
+          entityType: "CUSTOMER"
+        },
+        name: "A4B72CCDFF33",
+        type: "Temperature Sensor",
+        label: "Room 234 Sensor",
+        deviceProfileId: {
+          id: "784f394c-42b6-435a-983c-b7beff2784f9",
+          entityType: "DEVICE_PROFILE"
+        },
+        deviceData: {
+          configuration: {},
+          transportConfiguration: {}
+        },
+        firmwareId: {
+          id: "784f394c-42b6-435a-983c-b7beff2784f9",
+          entityType: "OTA_PACKAGE"
+        },
+        softwareId: {
+          id: "784f394c-42b6-435a-983c-b7beff2784f9",
+          entityType: "OTA_PACKAGE"
+        },
+        additionalInfo: {},
+      },
+      headers: {},
+      config: {},
+      status: 200,
+      statusText: 'OK'
+    }
+
+    jest.spyOn(httpService, 'post').mockImplementationOnce(() => of(result));
+    const custID = "784f394c-42b6-435a-983c-b7beff2784f9";
+    const deviceID = "fa0097e0-dfaa-11ec-b99c-f7477a3db362";
+    console.log(await service.assignDevicetoCustomer(custID, deviceID));
     /*const data = await loginService.login(username, password);
     service.setToken(data['data']['token']);
     expect(await service.assignDevicetoCustomer("9fed2a30-dfa9-11ec-b99c-f7477a3db362", "fa0097e0-dfaa-11ec-b99c-f7477a3db362")).toEqual(true);*/
