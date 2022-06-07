e8c63c52d37d4387e4571d6d06da8e5a087e63e8
Brandon Kruger <u18019499@tuks.co.za>
2022-06-07 16:28:02 +0200

    :bug: (UI): Remove empty functions

--- master/libs/client/shared-ui/components-ui/src/lib/header-ui/header-ui.component.ts
+++ master/libs/client/shared-ui/components-ui/src/lib/header-ui/header-ui.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'master-header-nav',
   templateUrl: './header-ui.component.html',
   styleUrls: ['./header-ui.component.scss'],
 })
-export class HeaderUiComponent implements OnInit {
+export class HeaderUiComponent{
 
   logoLoc = "assets/Logos/navlogo.svg";
 
-  constructor() {}
-
-  ngOnInit(): void {}
 }
