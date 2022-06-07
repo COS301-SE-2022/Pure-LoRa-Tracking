4b9861ba584cdd0de3cc433b67025f15f9a20b04
t3ddy33 <u20493836@tuks.co.za>
2022-06-07 21:25:57 +0200

    ✅ (testing) Added add gateway device mock test.

--- master/libs/api/device-endpoint/src/lib/api-device-endpoint.service.spec.ts
+++ master/libs/api/device-endpoint/src/lib/api-device-endpoint.service.spec.ts
@@ -133,7 +133,7 @@ describe('ApiDeviceEndpointService', () => {
     expect(result).toBeDefined();
   });
 
-  it('should process a device, add it to a specified reserve, and return a confirmation message', async () =>{
+  it('should process a sensor device, add it to a specified reserve, and return a confirmation message', async () =>{
     const bodyData = {
       token: "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0ZW5hbnRAdGhpbmdzYm9hcmQub3JnIi...",
       customerID: "784f394c-42b6-435a-983c-b7beff2784f9",
@@ -225,6 +225,133 @@ describe('ApiDeviceEndpointService', () => {
         "name": "A4B72CCDFF33",
         "type": "Sensor",
         "label": "Room 234 Sensor",
+        "deviceProfileId": {
+          "id": "784f394c-42b6-435a-983c-b7beff2784f9",
+          "entityType": "DEVICE_PROFILE"
+        },
+        "deviceData": {
+          "configuration": {},
+          "transportConfiguration": {}
+        },
+        "firmwareId": {
+          "id": "784f394c-42b6-435a-983c-b7beff2784f9",
+          "entityType": "OTA_PACKAGE"
+        },
+        "softwareId": {
+          "id": "784f394c-42b6-435a-983c-b7beff2784f9",
+          "entityType": "OTA_PACKAGE"
+        },
+        "additionalInfo": {}
+      },
+      headers: {},
+      config: {},
+      status: 200,
+      statusText: 'OK'
+    }
+
+    jest.spyOn(httpService, 'get').mockImplementationOnce(() => of(result));
+    jest.spyOn(httpService, 'get').mockImplementationOnce(() => of(secondResult));
+    jest.spyOn(httpService, 'get').mockImplementationOnce(() => of(thirdResult));
+    jest.spyOn(httpService, 'post').mockImplementationOnce(() => of(fourthResult));
+    jest.spyOn(httpService, 'post').mockImplementationOnce(() => of(fourthResult));
+
+    const response = await service.processDeviceAddsensor(bodyData);
+    console.log(response);
+    expect(response).toBeDefined();
+  });
+
+it('should process a gateway device, add it to a specified reserve, and return a confirmation message', async () =>{
+    const bodyData = {
+      token: "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0ZW5hbnRAdGhpbmdzYm9hcmQub3JnIi...",
+      customerID: "784f394c-42b6-435a-983c-b7beff2784f9",
+      hardwareName: "A8B76FCDDF55",
+      labelName: "Reserve gateway",
+    }
+    const result: AxiosResponse<any> = {
+      data: {
+        "token": "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0ZW5hbnRAdGhpbmdzYm9hcmQub3JnIi...",
+        "refreshToken": "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0ZW5hbnRAdGhpbmdzYm9hcmQub3JnIi..."
+      },
+      headers: {},
+      config: {},
+      status: 200,
+      statusText: 'OK'
+    }
+
+    const secondResult: AxiosResponse<any> = {
+      data : {
+        "id": {
+          "id": "784f394c-42b6-435a-983c-b7beff2784f9",
+          "entityType": "USER"
+        },
+        "createdTime": 1609459200000,
+        "tenantId": {
+          "id": "784f394c-42b6-435a-983c-b7beff2784f9",
+          "entityType": "TENANT"
+        },
+        "customerId": {
+          "id": "784f394c-42b6-435a-983c-b7beff2784f9",
+          "entityType": "CUSTOMER"
+        },
+        "email": "user@example.com",
+        "name": "user@example.com",
+        "authority": "TENANT_ADMIN",
+        "firstName": "John",
+        "lastName": "Doe",
+        "additionalInfo": {}
+      },
+      headers: {},
+      config: {},
+      status: 200,
+      statusText: 'OK'
+    }
+
+    const thirdResult: AxiosResponse<any> = {
+      data: {
+        "id": {
+          "id": "784f394c-42b6-435a-983c-b7beff2784f9",
+          "entityType": "CUSTOMER"
+        },
+        "createdTime": 1609459200000,
+        "title": "Company A",
+        "name": "Company A",
+        "tenantId": {
+          "id": "784f394c-42b6-435a-983c-b7beff2784f9",
+          "entityType": "TENANT"
+        },
+        "country": "US",
+        "state": "NY",
+        "city": "New York",
+        "address": "42 Broadway Suite 12-400",
+        "address2": "string",
+        "zip": "10004",
+        "phone": "+1(415)777-7777",
+        "email": "example@company.com",
+        "additionalInfo": {}
+      },
+      headers: {},
+      config: {},
+      status: 200,
+      statusText: 'OK'
+    }
+    const fourthResult: AxiosResponse<any> = {
+      data: {
+        "id": {
+          "id": "784f394c-42b6-435a-983c-b7beff2784f9",
+          "entityType": "DEVICE"
+        },
+        "createdTime": 1609459200000,
+        "tenantId": {
+          "id": "784f394c-42b6-435a-983c-b7beff2784f9",
+          "entityType": "TENANT"
+        },
+        "customerId": {
+          "id": "784f394c-42b6-435a-983c-b7beff2784f9",
+          "entityType": "CUSTOMER"
+        },
+        "name": "A8B76FCDDF55",
+        "type": "Gateway",
+        "label": "Reserve gateway",
         "deviceProfileId": {
           "id": "784f394c-42b6-435a-983c-b7beff2784f9",
           "entityType": "DEVICE_PROFILE"
