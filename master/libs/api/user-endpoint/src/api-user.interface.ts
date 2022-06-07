7ed2f937aa8e1d8eebffa256ce7c730f13d70279
LiamBurgess18015001 <u18015001@tuks.co.za>
2022-06-07 11:06:43 +0200

    🎨 (api): user interface

---
+++ master/libs/api/user-endpoint/src/api-user.interface.ts
@@ -0,0 +1,20 @@
+export interface userAddInput {}
+
+export interface userRemoveInput {}
+
+export interface userDisableInput {}
+
+export interface userEnableInput {}
+
+export interface userInfoInput {}
+
+export interface userAddResponse {}
+
+export interface userRemoveResponse {}
+
+export interface userDisableResponse {}
+
+export interface userEnableResponse {}
+
+export interface userInfoResponse {}
+
