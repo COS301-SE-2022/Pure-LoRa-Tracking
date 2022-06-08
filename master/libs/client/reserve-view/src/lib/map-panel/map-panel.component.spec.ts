d9da95559da6a04a4c712b80ddd94bbc4f219783
MildogMiller <37893674+MildogMiller@users.noreply.github.com>
2022-06-08 14:54:37 +0200

    🐛 fixed jest error

--- master/libs/client/reserve-view/src/lib/map-panel/map-panel.component.spec.ts
+++ master/libs/client/reserve-view/src/lib/map-panel/map-panel.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MapPanelComponent } from './map-panel.component';
+import {MatButtonToggleModule} from "@angular/material/button-toggle"
 
 describe('MapPanelComponent', () => {
   let component: MapPanelComponent;
@@ -8,6 +9,7 @@ describe('MapPanelComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports:[MatButtonToggleModule],
       declarations: [MapPanelComponent],
     }).compileComponents();
   });
