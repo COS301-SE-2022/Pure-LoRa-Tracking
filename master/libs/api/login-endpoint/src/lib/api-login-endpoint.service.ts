a4a8535fa5050813683ad2610dd337f385c25517
t3ddy33 <u20493836@tuks.co.za>
2022-06-08 22:32:51 +0200

    🔨 (dev): Implemented login function in login api.

--- master/libs/api/login-endpoint/src/lib/api-login-endpoint.service.ts
+++ master/libs/api/login-endpoint/src/lib/api-login-endpoint.service.ts
@@ -1,8 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import { ThingsboardThingsboardClientService } from '@lora/thingsboard-client';
+import { userLoginData } from '../api-login.interface';
 
 @Injectable()
 export class ApiLoginEndpointService {
     constructor(private thingsboardClient: ThingsboardThingsboardClientService) { }
-    
+    async doLogin(body: userLoginData) {
+        if (body.username == undefined || body.password == undefined) {
+            return {
+                status: 400,
+                explain: 'Username and password required.'
+            }
+        }
+        const loginResponse = await this.thingsboardClient.loginUserReturnToken(body.username, body.password);
+        if (loginResponse.Token != "" && loginResponse.refreshToken != "") {
+            return {
+                status: 200,
+                explain: 'Login successful.',
+                token: loginResponse.Token,
+                refreshToken: loginResponse.refreshToken
+            }
+        }
+        return {
+            status: 401,
+            explain: 'Login unsuccessful.'
+        }
+    }
 }
