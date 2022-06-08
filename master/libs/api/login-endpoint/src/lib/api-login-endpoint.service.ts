18d4d127c2ea9f460d2d1cdc195e6f701a109173
t3ddy33 <u20493836@tuks.co.za>
2022-06-08 23:33:25 +0200

    🔨 (dev): Updated doLogin.

--- master/libs/api/login-endpoint/src/lib/api-login-endpoint.service.ts
+++ master/libs/api/login-endpoint/src/lib/api-login-endpoint.service.ts
@@ -1,19 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import { ThingsboardThingsboardClientService } from '@lora/thingsboard-client';
-import { userLoginData } from '../api-login.interface';
+import { userLoginData, userLoginResponse } from '../api-login.interface';
+import { execFile } from 'child_process';
 
 @Injectable()
 export class ApiLoginEndpointService {
     constructor(private thingsboardClient: ThingsboardThingsboardClientService) { }
-    async doLogin(body: userLoginData) {
+
+    async doLogin(body: userLoginData): Promise<userLoginResponse> {
         if (body.username == undefined || body.password == undefined) {
             return {
                 status: 400,
                 explain: 'Username and password required.'
             }
         }
-        const loginResponse = await this.thingsboardClient.loginUserReturnToken(body.username, body.password);
+        const loginResponse = await this.thingsboardClient.loginUserReturnToken(body.username, body.password)
+            .catch(() => {
+                return {
+                    Token: "",
+                    refreshToken: "",
+                }
+            });
         if (loginResponse.Token != "" && loginResponse.refreshToken != "") {
+            this.thingsboardClient.setToken(loginResponse.Token)
             return {
                 status: 200,
                 explain: 'Login successful.',
