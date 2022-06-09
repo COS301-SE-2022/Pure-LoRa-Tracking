cbc04e55a64761cbbd1ddafc68e900a15d647c74
Brandon Kruger <u18019499@tuks.co.za>
2022-06-09 15:44:07 +0200

    :art: (UI): User List

--- master/libs/client/users-management-view/src/lib/client-users-management-view.module.ts
+++ master/libs/client/users-management-view/src/lib/client-users-management-view.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { ReserveUsersViewComponent } from './reserve-users-view/reserve-users-view.component';
+import { ClientSharedUiComponentsUiModule } from '@master/client/shared-ui/components-ui';
+import { ClientSharedUiMaterialUiModule } from '@master/client/shared-ui/material-ui';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatTableModule} from '@angular/material/table';
 
 @NgModule({
   imports: [
     CommonModule,
-
+    ClientSharedUiComponentsUiModule,
+    ClientSharedUiMaterialUiModule,
+    MatSlideToggleModule,
+    MatTableModule,
     RouterModule.forChild([
-      /* {path: '', pathMatch: 'full', component: InsertYourComponentHere} */
+       {path: '', pathMatch: 'full', component: ReserveUsersViewComponent} 
     ]),
   ],
   declarations: [ReserveUsersViewComponent],
--- master/libs/client/users-management-view/src/lib/reserve-users-view/reserve-users-view.component.ts
+++ master/libs/client/users-management-view/src/lib/reserve-users-view/reserve-users-view.component.ts
@@ -1,12 +1,67 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface userInfo{
+  name: string,
+  surname: string,
+  id: string,
+  email: string,
+  status: boolean,
+}
+const usersList:userInfo[] = [
+  {
+    name: "James",
+    surname: "Johnson",
+    email: "james@gmail.com",
+    id: "15454",
+    status: true,
+  },
+  {
+    name: "Piet",
+    surname: "Piet",
+    email: "james@gmail.com",
+    id: "454545646",
+    status: false,
+  },
+  {
+    name: "Kim",
+    surname: "Can",
+    email: "kim@gmail.com",
+    id: "74657456456",
+    status: true,
+  },
+  {
+    name: "Jack",
+    surname: "Man",
+    email: "jack@gmail.com",
+    id: "45456456456",
+    status: false,
+  }
+]
 @Component({
   selector: 'master-reserve-users-view',
   templateUrl: './reserve-users-view.component.html',
   styleUrls: ['./reserve-users-view.component.scss'],
 })
 export class ReserveUsersViewComponent implements OnInit {
+    
+  tableColumns:string[] = ['id', 'surname', 'name','email',"status","delete"];
+  sourceData = usersList;
+  addUser= false;
   constructor() {}
 
   ngOnInit(): void {}
+
+  removeUser(userId:string): void {
+    console.log(userId);
+  }
+
+  changeUserStatus(userId:string): void {
+      console.log(userId);
+    
+  }
+
+  openUserForm(): void {
+    this.addUser = true;
+  }
+
 }
