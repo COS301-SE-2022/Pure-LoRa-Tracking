1758a3886cea789c87cb240fd26ca4e783091117
LiamBurgesss <u18015001@tuks.co.za>
2022-06-29 13:13:59 +0200

    ✨ (middleware) : body processing

--- master/libs/middleware/session-management/src/lib/middleware-session-management.service.ts
+++ master/libs/middleware/session-management/src/lib/middleware-session-management.service.ts
@@ -1,9 +1,14 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
+import { ThingsboardThingsboardClientService } from "@lora/thingsboard-client";
+import { ServerResponse } from 'http';
 
 @Injectable()
 export class MiddlewareSessionManagementService implements NestMiddleware {
-    use(req: any, res: any, next: (error?: any) => void) {
-        console.log('session manage');
-        next();
+    constructor(private TBClient : ThingsboardThingsboardClientService) {}
+    use(req: Request, res: ServerResponse, next: (error?: any) => void) {
+        if(req.body['token'] == undefined)
+           return res.end()
+        else
+            next();
     }
 }
