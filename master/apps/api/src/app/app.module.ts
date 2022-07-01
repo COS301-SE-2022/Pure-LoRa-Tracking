a316118f02d10218dec9cbc050bdb67269fad336
LiamBurgesss <u18015001@tuks.co.za>
2022-07-01 14:17:10 +0200

    ✨ (database): mongodb insert implemented

--- master/apps/api/src/app/app.module.ts
+++ master/apps/api/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { ApiApiRouterModule } from '@lora/apiRouter'
-import { DatabaseProxyModule } from '@lora/database'
 import { MiddlewareHttpLoggerService } from '@lora/middleware/logger';
 import { MiddlewareSessionManagementService } from '@lora/middleware/session-management';
 
--- master/libs/database-proxy/src/lib/database-proxy.service.ts
+++ master/libs/database-proxy/src/lib/database-proxy.service.ts
@@ -1,6 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { DataInput, InputDataDocument } from '../database-interfaces.interface';
 
 @Injectable()
 export class DatabaseProxyService {
-    
+    constructor(@InjectModel(DataInput.name) private DataModel: Model<InputDataDocument>) {}
+
+    insertLocation(dataToInsert : DataInput) {
+        const Insert = new this.DataModel(dataToInsert);
+        return Insert.save();
+    }
 }
