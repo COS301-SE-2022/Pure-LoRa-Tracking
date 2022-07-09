2870afcb6f45e3ee77458e3eab8707d2b9060071
t3ddy33 <u20493836@tuks.co.za>
2022-07-09 22:42:55 +0200

    🔨 (dev): Added function to get customers.

--- master/libs/thingsboard/thingsboard-admin/src/lib/thingsboard-thingsboard-admin.service.ts
+++ master/libs/thingsboard/thingsboard-admin/src/lib/thingsboard-thingsboard-admin.service.ts
@@ -1,4 +1,67 @@
 import { Injectable } from '@nestjs/common';
-
+import { HttpService } from '@nestjs/axios';
+import { firstValueFrom } from 'rxjs';
 @Injectable()
-export class ThingsboardThingsboardAdminService {}
+export class ThingsboardThingsboardAdminService {
+  private ThingsBoardURL = process.env.TB_URL || 'http://localhost:8080/api';
+  constructor(private httpService: HttpService) {}
+
+  async getCustomersOfTenant(
+    token: string,
+    pageSize: number,
+    page: number,
+    textSearch?: string,
+    sortProperty?: string,
+    sortOrder?: string
+  ): Promise<UserResponse> {
+    const headersReq = {
+      'Content-Type': 'application/json',
+      Authorization: 'Bearer ' + token,
+    };
+
+    let args: string = 'pageSize=' + pageSize + '&' + 'page=' + page;
+    args += textSearch ? '&textSearch=' + textSearch : '';
+    args += sortProperty ? '&sortProperty=' + sortProperty : '';
+    args += sortOrder ? '&sortProperty=' + sortOrder : '';
+
+    const resp = await firstValueFrom(
+      this.httpService.get(this.ThingsBoardURL + '/api/customers?' + args, {
+        headers: headersReq,
+      })
+    ).catch((error) => {
+      if (error.response == undefined) return error.code;
+      return error;
+    });
+  }
+}
+
+export interface UserResponse {
+  status: number;
+  explanation: string;
+  data?: {
+    token?: string;
+    refreshToken?: string;
+    id?: {
+      id?: string;
+      entityType?: string;
+    };
+    createdTime?: number;
+    tenantId?: {
+      id?: string;
+      entityType?: string;
+    };
+    customerId?: {
+      id?: string;
+      entityType?: string;
+    };
+    email?: string;
+    name?: string;
+    authority?: 'SYS_ADMIN' | 'TENANT_ADMIN' | 'CUSTOMER_USER';
+    firstName?: string;
+    lastName?: string;
+    additionalInfo?: {
+      reserves?: string[];
+    };
+  };
+  type?: string;
+}
