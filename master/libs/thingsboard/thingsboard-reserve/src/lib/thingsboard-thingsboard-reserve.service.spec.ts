19a7552d878cf6b899ada002673d7d3342d45bff
t3ddy33 <u20493836@tuks.co.za>
2022-07-09 22:11:22 +0200

    ✅ (testing): Added tb-reserve tests

--- master/libs/thingsboard/thingsboard-reserve/src/lib/thingsboard-thingsboard-reserve.service.spec.ts
+++ master/libs/thingsboard/thingsboard-reserve/src/lib/thingsboard-thingsboard-reserve.service.spec.ts
@@ -15,4 +15,14 @@ describe('ThingsboardThingsboardReserveService', () => {
   it('should be defined', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should create the reserve group', async () => {
+    /*const Login = await service.login(username, password);
+    console.log(await service.createReserveGroup(Login["data"]["token"], "liamburgess299@gmail.com", "reserveb"));*/
+  });
+
+  it('should delete the reserve group', async () => {
+    /*const Login = await service.login(username, password);
+    console.log(await service.deleteReserveGroup(Login["data"]["token"], "573911a0-e5b2-11ec-a9e5-f30a5c07bcf3"));*/
+  });
 });
