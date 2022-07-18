82d23d7a2da6da76cb9f5ef758d5c19d7a0fe7d4
MildogMiller <37893674+MildogMiller@users.noreply.github.com>
2022-07-18 17:51:59 +0200

    ✅ added more tests

--- master/libs/client/reserve-view/src/lib/map-panel/map-panel.component.spec.ts
+++ master/libs/client/reserve-view/src/lib/map-panel/map-panel.component.spec.ts
@@ -2,10 +2,16 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MapPanelComponent } from './map-panel.component';
 import {MatButtonToggleModule} from "@angular/material/button-toggle"
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 describe('MapPanelComponent', () => {
   let component: MapPanelComponent;
   let fixture: ComponentFixture<MapPanelComponent>;
+
+  const demodate= new FormGroup({
+    startdateform: new FormControl(new Date(),[Validators.required]),
+    enddateform: new FormControl(new Date(),[Validators.required]),
+  });
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -23,4 +29,30 @@ describe('MapPanelComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+
+  describe("Reset",()=>{
+    beforeEach(()=>{
+      jest.spyOn(component.daterange,"setValue").mockImplementation();
+      jest.spyOn(component,"apply").mockImplementation();
+    })
+    it("Should reset the start and end time",()=>{
+      component.reset();
+      expect(component.starttime).toEqual("0");
+      expect(component.endtime).toEqual("23");
+    })
+    it("Should reset the date values",()=>{
+      component.reset();
+      expect(component.daterange.setValue).toBeCalled();
+    })
+    it("Should call the apply function",()=>{
+      component.reset();
+      expect(component.apply).toBeCalled();
+    })
+  })
+
+  describe("Apply",()=>{
+    
+  })
+
 });
