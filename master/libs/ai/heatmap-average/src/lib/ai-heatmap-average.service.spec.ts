be37aebe3e3297408cb7e06052ee1586b6ede3e5
LiamBurgess18015001 <u18015001@tuks.co.za>
2022-07-22 10:45:38 +0200

    ✨ (ai): averaging info

---
+++ master/libs/ai/heatmap-average/src/lib/ai-heatmap-average.service.spec.ts
@@ -0,0 +1,18 @@
+import { Test } from '@nestjs/testing';
+import { AiHeatmapAverageService } from './ai-heatmap-average.service';
+
+describe('AiHeatmapAverageService', () => {
+  let service: AiHeatmapAverageService;
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [AiHeatmapAverageService],
+    }).compile();
+
+    service = module.get(AiHeatmapAverageService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeTruthy();
+  });
+});
