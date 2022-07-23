d941e3ae4e256cbb657302bdade686e756f8d53a
LiamBurgesss <u18015001@tuks.co.za>
2022-07-23 09:01:26 +0200

    ✨ (ai): lat-long conversions

--- master/libs/ai/heatmap-average/src/lib/ai-heatmap-average.service.ts
+++ master/libs/ai/heatmap-average/src/lib/ai-heatmap-average.service.ts
@@ -1,4 +1,40 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AiHeatmapAverageService {}
+export class AiHeatmapAverageService {
+    private EarthRadius  = 6371;
+
+    LatLongToGeometric(latitude:number, longitude: number) : {x:number,y:number,z:number} {
+        return {
+            x : Math.cos(latitude) * Math.cos(longitude),
+            y : Math.cos(latitude) * Math.sin(longitude),
+            z : Math.sin(latitude)
+        }
+    }
+
+    GeometricAverage(GeomSet : {x:number,y:number,z:number}[]) : {x:number,y:number,z:number} {
+        let Xtot: number, Ytot: number, Ztot : number;
+        Xtot = 0;
+        Ytot = 0;
+        Ztot = 0;
+        GeomSet.forEach(item => {
+            Xtot += item.x;
+            Ytot += item.y;
+            Ztot += item.z;
+        })
+
+        return {
+            x : Xtot,
+            y : Ytot,
+            z : Ztot
+        }
+    }
+
+    GeometricToLatLong(GeomSet : {x:number,y:number,z:number}) : {latitude:number, longitude:number} {
+        const hyp = Math.sqrt(GeomSet.x * GeomSet.x + GeomSet.y * GeomSet.y);
+        return {
+            longitude : Math.atan2(GeomSet.y, GeomSet.x),
+            latitude : Math.atan2(GeomSet.z, hyp)
+        }
+    }
+}
