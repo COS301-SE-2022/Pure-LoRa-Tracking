97733a237d8295ef489b36d06b2ff29cd70887f7
Brandon Kruger <u18019499@tuks.co.za>
2022-07-24 12:18:27 +0200

    🚨 (UI): Fix linting errors

--- master/libs/client/manage-view/src/lib/client-manage-view.module.ts
+++ master/libs/client/manage-view/src/lib/client-manage-view.module.ts
@@ -7,13 +7,13 @@ import { ClientSharedUiComponentsUiModule } from '@master/client/shared-ui/compo
 import {MatSidenavModule} from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { ReserveDetailComponent } from './reserve-detail-view/src/lib/reserve-detail/reserve-detail.component';
-import { ProfilesViewComponent } from './profile-view/src/lib/profiles-view/profiles-view.component';
-import { ProfilesEditComponent } from './profile-edit/src/lib/profiles-edit/profiles-edit.component';
-import { UserViewComponent } from './users-view/src/lib/user-view/user-view.component';
-import { ReserveCreateComponent } from './create-reserve/src/lib/reserve-create/reserve-create.component';
-import { ReserveEditComponent } from './reserve-edit/src/lib/reserve-edit/reserve-edit.component';
-import { UserEditComponent } from './users-edit/src';
+import { ReserveDetailComponent } from '@master/client/manage-view/src/lib/reserve-detail-view';
+import { ProfilesViewComponent } from '@master/client/manage-view/src/lib/profile-view';
+import { ProfilesEditComponent } from '@master/client/manage-view/src/lib/profile-edit';
+import { UserViewComponent } from '@master/client/manage-view/src/lib/users-view';
+import { ReserveCreateComponent } from '@master/client/manage-view/src/lib/create-reserve';
+import { ReserveEditComponent } from '@master/client/manage-view/src/lib/reserve-edit';
+import { UserEditComponent } from '@master/client/manage-view/src/lib/users-edit';
 
 export const clientManageViewRoutes: Route[] = [
   {
--- master/libs/client/manage-view/src/lib/users-view/src/lib/user-view/user-view.component.ts
+++ master/libs/client/manage-view/src/lib/users-view/src/lib/user-view/user-view.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'master-profiles-edit',
   templateUrl: './user-view.component.html',
   styleUrls: ['./user-view.component.scss'],
 })
-export class UserViewComponent implements OnInit {
-  constructor() {}
+export class UserViewComponent {
+  constructor() {
+    console.log("Users view")
+  }
 
-  ngOnInit(): void {}
 }
