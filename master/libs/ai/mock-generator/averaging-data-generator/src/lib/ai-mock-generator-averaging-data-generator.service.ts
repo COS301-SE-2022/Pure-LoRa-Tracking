ef6104ac844c3a4822a8daaa2f1156ac2ac1904e
t3ddy33 <u20493836@tuks.co.za>
2022-07-26 01:00:54 +0200

    🔨 (ai): Implement generate false points

--- master/libs/ai/mock-generator/averaging-data-generator/src/lib/ai-mock-generator-averaging-data-generator.service.ts
+++ master/libs/ai/mock-generator/averaging-data-generator/src/lib/ai-mock-generator-averaging-data-generator.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AiMockGeneratorAveragingDataGeneratorService {
   /*  Assuming a box-border is used.
-      Format : (longitude, latitude) */
+      Format : [longitude, latitude] */
   private bounds = [
     [28.235185146331787, -25.752202889891826],
     [28.228425979614258, -25.752077267386465],
@@ -14,6 +14,9 @@ export class AiMockGeneratorAveragingDataGeneratorService {
   private decimals = 15;
 
   private trainData: AverageInputInterface;
+
+  private variationFactor = 1000;
+  private deviationFactor = 0.3;
 
   getRandomFloat(min, max) {
     return parseFloat(
@@ -30,6 +33,33 @@ export class AiMockGeneratorAveragingDataGeneratorService {
     const latitude = this.getRandomFloat(b_lat, t_lat);
     this.trainData.truePoint.latitude = latitude;
     this.trainData.truePoint.longitude = longitude;
+  }
+
+  generateFalsePoints(numberOfSamples: number) {
+    let nTimestamp: string;
+    let nLongitude, nLatitude;
+
+    for (let i = 0; i < numberOfSamples; i++) {
+      nTimestamp = (Date.now() + 60 * 1000 * i).toString();
+      nLongitude =
+        this.getRandomFloat(
+          this.trainData.truePoint.longitude * this.variationFactor,
+          this.trainData.truePoint.longitude * this.variationFactor +
+            this.deviationFactor
+        ) / this.variationFactor;
+      nLatitude =
+        this.getRandomFloat(
+          this.trainData.truePoint.latitude * this.variationFactor,
+          this.trainData.truePoint.latitude * this.variationFactor +
+            this.deviationFactor
+        ) / this.variationFactor;
+
+      this.trainData.coordinates.push({
+        latitude: nLatitude,
+        longitude: nLongitude,
+        timestamp: nTimestamp,
+      });
+    }
   }
 }
 
