b2fc571bfecb89055d1c7011c9899a9c45806898
t3ddy33 <u20493836@tuks.co.za>
2022-07-26 00:16:04 +0200

    🔨 (ai): Add average input interface and bounds

--- master/libs/ai/mock-generator/averaging-data-generator/src/lib/ai-mock-generator-averaging-data-generator.service.ts
+++ master/libs/ai/mock-generator/averaging-data-generator/src/lib/ai-mock-generator-averaging-data-generator.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AiMockGeneratorAveragingDataGeneratorService {}
+export class AiMockGeneratorAveragingDataGeneratorService {
+  /*  Assuming a box-border is used.
+      Format : (longitude, latitude) */
+  private bounds = [
+    [28.235185146331787, -25.752202889891826],
+    [28.228425979614258, -25.752077267386465],
+    [28.227782249450684, -25.756155099326918],
+    [28.235281705856323, -25.75700543526357],
+  ];
+}
+
+export interface AverageInputInterface {
+  sensorID: string;
+  coordinates: {
+    latitude: number;
+    longitude: number;
+    timestamp: string;
+  }[];
+  truePoint: {
+    latitude: number;
+    longitude: number;
+  };
+}
