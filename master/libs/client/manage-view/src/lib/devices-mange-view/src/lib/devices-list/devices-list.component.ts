92ea929dd0143d25693a56062a450c9530de8317
Brandon Kruger <u18019499@tuks.co.za>
2022-07-27 20:30:36 +0200

    :art: (UI): Inline device edit

--- master/libs/client/manage-view/src/lib/devices-mange-view/src/lib/devices-list/devices-list.component.ts
+++ master/libs/client/manage-view/src/lib/devices-mange-view/src/lib/devices-list/devices-list.component.ts
@@ -7,28 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DevicesListComponent implements OnInit {
 
-  tableColumns:string[] = ['id', 'name',"status","delete","edit"];
+  tableColumns:string[] = ["id", "name","reserve","status","delete","edit"];
   sensorData=[{
     id: "abc",
     name: "bcd",
+    reserve: "2",
     status: "active"
   },{
     id: "fcd",
     name: "dfg",
+    reserve: "3",
     status: "inactive"
   }];
 
   gatewayData=[{
     id: "as",
     name: "sd",
+    reserve: "1",
     status: "active"
   },{
     id: "fcdfd",
     name: "dfd",
+    reserve: "2",
     status: "inactive"
   }];
 
-  reserveList=[{name:"A", id:1},{name:"B",id:2},{name:"C",id:3}];
+  reserveList=[{name:"A", id:"1"},{name:"B",id:"2"},{name:"C",id:"3"}];
 
   constructor() {}
 
@@ -45,5 +49,13 @@ export class DevicesListComponent implements OnInit {
 
   gatewayReserveFilter(event:any): void {
     console.log(event);
+  }
+
+  changeDeviceStatus(event:any, type:string, id: string): void {
+    console.log(type + ": "+ id +" changed to "+event.value);
+  }
+
+  reassignDevice(event:any, type:string, id: string): void {
+
   }
 }
