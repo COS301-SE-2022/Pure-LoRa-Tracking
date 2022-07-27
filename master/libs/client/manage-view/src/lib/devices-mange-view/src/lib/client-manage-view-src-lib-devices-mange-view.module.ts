6a9f768894ffc878c0c301ca68225cff402daaba
Brandon Kruger <u18019499@tuks.co.za>
2022-07-27 19:51:15 +0200

    :art: (UI):  Devices List

--- master/libs/client/manage-view/src/lib/devices-mange-view/src/lib/client-manage-view-src-lib-devices-mange-view.module.ts
+++ master/libs/client/manage-view/src/lib/devices-mange-view/src/lib/client-manage-view-src-lib-devices-mange-view.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Route } from '@angular/router';
 import { DevicesListComponent } from './devices-list/devices-list.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatSelectModule} from '@angular/material/select';
 
 export const clientManageViewSrcLibDevicesMangeViewRoutes: Route[] = [];
 
 @NgModule({
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, MatTableModule, MatIconModule, MatButtonModule, MatTabsModule, MatSelectModule],
   declarations: [DevicesListComponent],
   exports: [DevicesListComponent],
 })
--- master/libs/client/manage-view/src/lib/devices-mange-view/src/lib/devices-list/devices-list.component.ts
+++ master/libs/client/manage-view/src/lib/devices-mange-view/src/lib/devices-list/devices-list.component.ts
@@ -6,7 +6,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./devices-list.component.scss'],
 })
 export class DevicesListComponent implements OnInit {
+
+  tableColumns:string[] = ['id', 'name',"status","delete","edit"];
+  sensorData=[{
+    id: "abc",
+    name: "bcd",
+    status: "active"
+  },{
+    id: "fcd",
+    name: "dfg",
+    status: "inactive"
+  }];
+
+  gatewayData=[{
+    id: "as",
+    name: "sd",
+    status: "active"
+  },{
+    id: "fcdfd",
+    name: "dfd",
+    status: "inactive"
+  }];
+
+  reserveList=[{name:"A", id:1},{name:"B",id:2},{name:"C",id:3}];
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+  }
+
+  deleteDevice(id:string):void {
+    console.log("Delete: " + id);
+  }
+
+  sensorReserveFilter(event:any): void {
+    console.log(event);
+  }
+
+  gatewayReserveFilter(event:any): void {
+    console.log(event);
+  }
 }
