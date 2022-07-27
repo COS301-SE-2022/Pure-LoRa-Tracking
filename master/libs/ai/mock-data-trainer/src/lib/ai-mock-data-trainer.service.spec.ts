466ba81e4e72ed006b5f8e8e12684f221cd4a21d
t3ddy33 <u20493836@tuks.co.za>
2022-07-27 20:50:20 +0200

    🔨 (ai): Add mock data trainer lib

---
+++ master/libs/ai/mock-data-trainer/src/lib/ai-mock-data-trainer.service.spec.ts
@@ -0,0 +1,18 @@
+import { Test } from '@nestjs/testing';
+import { AiMockDataTrainerService } from './ai-mock-data-trainer.service';
+
+describe('AiMockDataTrainerService', () => {
+  let service: AiMockDataTrainerService;
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [AiMockDataTrainerService],
+    }).compile();
+
+    service = module.get(AiMockDataTrainerService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeTruthy();
+  });
+});
