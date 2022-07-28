66a6d8577bf8ec6163719a99b72cef5e9a4f42fb
MildogMiller <37893674+MildogMiller@users.noreply.github.com>
2022-07-28 21:38:04 +0200

    🐛 fixed linting

--- master/libs/client/manage-view/src/lib/gateway-edit-view/src/lib/gateway-edit/gateway-edit.component.spec.ts
+++ master/libs/client/manage-view/src/lib/gateway-edit-view/src/lib/gateway-edit/gateway-edit.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import {HttpClientTestingModule} from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { GatewayEditComponent } from './gateway-edit.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 describe('GatewayEditComponent', () => {
   let component: GatewayEditComponent;
@@ -9,7 +10,7 @@ describe('GatewayEditComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule,HttpClientTestingModule],
+      imports: [RouterTestingModule,HttpClientTestingModule,ReactiveFormsModule],
       declarations: [GatewayEditComponent],
     }).compileComponents();
   });
