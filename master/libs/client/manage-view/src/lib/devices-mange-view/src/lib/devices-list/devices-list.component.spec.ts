64b321fbde2564d20c89fd2cc93ec93bbcb9c126
Brandon Kruger <u18019499@tuks.co.za>
2022-07-28 20:50:55 +0200

    :bug: (UI): Missing router test module

--- master/libs/client/manage-view/src/lib/devices-mange-view/src/lib/devices-list/devices-list.component.spec.ts
+++ master/libs/client/manage-view/src/lib/devices-mange-view/src/lib/devices-list/devices-list.component.spec.ts
@@ -2,13 +2,14 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import {HttpClientTestingModule} from '@angular/common/http/testing';
 import { DevicesListComponent } from './devices-list.component';
 
+import { RouterTestingModule } from '@angular/router/testing';
 describe('DevicesListComponent', () => {
   let component: DevicesListComponent;
   let fixture: ComponentFixture<DevicesListComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule],
+      imports: [HttpClientTestingModule, RouterTestingModule],
       declarations: [DevicesListComponent],
     }).compileComponents();
   });
