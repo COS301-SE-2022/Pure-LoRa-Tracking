a0510d4c97d7de7057f91fe7867ae9c66d3f6853
LiamBurgesss <u18015001@tuks.co.za>
2022-07-28 13:07:29 +0200

    ✨ (db): store and acquire averaging data

--- master/libs/database-proxy/src/lib/database-proxy.module.ts
+++ master/libs/database-proxy/src/lib/database-proxy.module.ts
@@ -1,12 +1,12 @@
 import { Module, Global } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { DatabaseProxyService } from './database-proxy.service';
-import { DataInput, InputDataSchema } from '../database-interfaces.interface';
+import { AverageInput, AverageInputSchema, } from '../database-interfaces.interface';
 
 @Global()
 @Module({
   imports : [MongooseModule.forRoot('mongodb://localhost/lora'),
-              MongooseModule.forFeature([{name:DataInput.name, schema:InputDataSchema}])
+              MongooseModule.forFeature([{name:AverageInput.name, schema:AverageInputSchema}])
 ],
   controllers: [],
   providers: [DatabaseProxyService],
