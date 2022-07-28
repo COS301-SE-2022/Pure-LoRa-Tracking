4dfec920becaf6e789fd07553374b5f04dd35518
t3ddy33 <u20493836@tuks.co.za>
2022-07-28 22:06:58 +0200

    🔨 (ai): Add error checking

--- master/libs/ai/mock-data-trainer/src/lib/ai-mock-data-trainer.service.ts
+++ master/libs/ai/mock-data-trainer/src/lib/ai-mock-data-trainer.service.ts
@@ -10,12 +10,27 @@ export class AiMockDataTrainerService {
     new AiHeatmapAverageService();
   private aiMockDataGenerator: AiMockGeneratorAveragingDataGeneratorService =
     new AiMockGeneratorAveragingDataGeneratorService();
-  private nEntries = 10;
+  private nEntries = 5;
   private nFalsePoints = 10;
+  private saveFilePath = 'file://libs/ai/Models/averaging';
+  private loadFilePath = 'file://libs/ai/Models/averaging/model.json';
 
-  @Cron('45 * * * * *')
+  //@Cron('45 * * * * *')
   async trainModel() {
-    await this.aiHeatMapService.loadModel();
+    const loadComplete = await this.aiHeatMapService.loadModel(
+      this.loadFilePath
+    );
+
+    if (!loadComplete) {
+      console.log(
+        '%c Failed to load from file. Building new model...',
+        'color: red;'
+      );
+      this.aiHeatMapService.buildModel();
+    } else {
+      console.log('%c Load from file successful.', 'color: green;');
+    }
+
     const learning = this.aiMockDataGenerator.generateData(
       this.nEntries,
       this.nFalsePoints
@@ -31,7 +46,16 @@ export class AiMockDataTrainerService {
       );
       await this.aiHeatMapService.fitModel(learn, train);
     }
-    await this.aiHeatMapService.saveModel();
+
+    const saveSuccessful = await this.aiHeatMapService.saveModel(
+      this.saveFilePath
+    );
+
+    if (!saveSuccessful) {
+      console.log('%c Save unsuccessful.', 'color: red;');
+    } else {
+      console.log('%c Save successful.', 'color: green;');
+    }
 
     const ToPredictData = this.aiHeatMapService.normalizePoints(
       this.aiHeatMapService.deconstructData(mock2[0].coordinates)
