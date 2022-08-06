045a5127e2506dfbff2ccc2b62f80379add3a8d6
LiamBurgesss <u18015001@tuks.co.za>
2022-08-06 10:44:19 +0200

    ✨ (processing): cron example

--- master/apps/data-processing/src/app/app.module.ts
+++ master/apps/data-processing/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { ProcessingApiRouterModule } from '@lora/processing/api/router';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { ScheduleModule } from '@nestjs/schedule';
+import { ProcessingBus } from '@processing/bus';
 
 @Module({
   imports: [
     ProcessingApiRouterModule,
     ScheduleModule.forRoot(),
     ConfigModule.forRoot(),
+    ProcessingBus,
     DatabaseProxyModule,
   ],
 })
--- master/libs/processing-api/bus/src/lib/processing-api-bus.service.ts
+++ master/libs/processing-api/bus/src/lib/processing-api-bus.service.ts
@@ -1,4 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { Cron, CronExpression } from '@nestjs/schedule';
 
 @Injectable()
-export class ProcessingBus {}
+export class ProcessingBus {
+    private readonly logger = new Logger(ProcessingBus.name);
+
+   /* @Cron(CronExpression.EVERY_30_SECONDS)
+    logCron() {
+        this.logger.verbose("CRON RUN")
+    }*/
+}
