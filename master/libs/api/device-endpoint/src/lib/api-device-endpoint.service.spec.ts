237b0b256f6d8e83316058e171cf0a5db66bfc3b
t3ddy33 <u20493836@tuks.co.za>
2022-08-07 15:51:21 +0200

    ✅ (testing): AddSensor -> undefined devProfileId

--- master/libs/api/device-endpoint/src/lib/api-device-endpoint.service.spec.ts
+++ master/libs/api/device-endpoint/src/lib/api-device-endpoint.service.spec.ts
@@ -224,6 +224,16 @@ describe('ApiDeviceEndpointService', () => {
     });
   });
 
+  it('processDeviceAddSensor -> undefined deviceProfileId', async () => {
+    delete tests.addSensorExampleInput.deviceProfileId;
+    expect(
+      await service.processDeviceAddsensor(tests.addSensorExampleInput)
+    ).toMatchObject({
+      status: 400,
+      explanation: 'no device Profile ID name found',
+    });
+  });
+
   // it('should process a sensor device, add it to a specified reserve, and return a confirmation message', async () => {
   //   const bodyData = {
   //     token:
