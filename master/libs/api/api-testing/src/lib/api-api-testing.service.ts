49ee4fc65d7e874bfbad02e6da23a19db55aacb7
t3ddy33 <u20493836@tuks.co.za>
2022-08-07 14:40:29 +0200

    🏷️ (testing): Add deviceInfosExample

--- master/libs/api/api-testing/src/lib/api-api-testing.service.ts
+++ master/libs/api/api-testing/src/lib/api-api-testing.service.ts
@@ -2,23 +2,27 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class ApiApiTestingService {
-    userEndpointExample = {
-        token: '123',
-        customerID: 'cid',
-        userInfo: {
-            email: 'email',
-            firstName: 'fname',
-            lastName: 'lname',
-        },
-        reserves: [{ reserveName: 'rname', reserveID: 'rid' }],
-        userID: 'uid',
-        refreshToken: "23",
-        reserveID: "12"
-    }
+  userEndpointExample = {
+    token: '123',
+    customerID: 'cid',
+    userInfo: {
+      email: 'email',
+      firstName: 'fname',
+      lastName: 'lname',
+    },
+    reserves: [{ reserveName: 'rname', reserveID: 'rid' }],
+    userID: 'uid',
+    refreshToken: '23',
+    reserveID: '12',
+  };
 
-    tbSuccess =
-        { status: 200, explain: "call finished" }
+  deviceInfosExample = {
+    token: 'SJNCKJWNJQQUIBWIU2837HJB1U12BJ',
+    customerID: '123',
+    deviceIDs: ['122', '123', '124'],
+  };
 
-    tbFail = 
-    { status : 500, explain : "ECONNREFUSED" }
+  tbSuccess = { status: 200, explain: 'call finished' };
+
+  tbFail = { status: 500, explain: 'ECONNREFUSED' };
 }
