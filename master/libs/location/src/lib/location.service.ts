6b3bc7cdd80106ac4d5f41911cb1410fa82f77a3
LiamBurgesss <u18015001@tuks.co.za>
2022-08-12 20:39:52 +0200

    ♻️ (data): location service refactor

--- master/libs/location/src/lib/location.service.ts
+++ master/libs/location/src/lib/location.service.ts
@@ -7,64 +7,62 @@ import * as trilat from 'trilat';
 export class LocationService {
     constructor(private tbClient: ThingsboardThingsboardClientService) { }
 
-    calculateLocation(gatewayData: UplinkRXInfo[], thingsBoardDeviceToken: string) {
-
-
+    
         //WGS 84 standard values
         // https://en.wikipedia.org/wiki/Geographic_coordinate_conversion
-        const EARTH_RADIUS = 6378137.0;  //a
-        const EARTH_RADIUS_SQR = EARTH_RADIUS * EARTH_RADIUS;  //a^2  asqr
-        const FLATTENING_DENOM = 298.257223563
-        const FLATTENING = 1 / FLATTENING_DENOM; //f
-        const POLAR_RADIUS = EARTH_RADIUS * (1 - FLATTENING);  //b -> 6356752.3142m
-        const POLAR_RADIUS_SQR = POLAR_RADIUS * POLAR_RADIUS; //b^2   bsqr
-        const E = Math.sqrt((EARTH_RADIUS_SQR - POLAR_RADIUS_SQR) / EARTH_RADIUS_SQR);  // 6.69437999014×10^−3
-        const E_PRIME = Math.sqrt((EARTH_RADIUS_SQR - POLAR_RADIUS_SQR) / POLAR_RADIUS_SQR); // 6.73949674228×10^−3
+        private EARTH_RADIUS = 6378137.0;  //a
+        private EARTH_RADIUS_SQR = this.EARTH_RADIUS * this.EARTH_RADIUS;  //a^2  asqr
+        private FLATTENING_DENOM = 298.257223563
+        private FLATTENING = 1 / this.FLATTENING_DENOM; //f
+        private POLAR_RADIUS = this.EARTH_RADIUS * (1 - this.FLATTENING);  //b -> 6356752.3142m
+        private POLAR_RADIUS_SQR = this.POLAR_RADIUS * this.POLAR_RADIUS; //b^2   bsqr
+        private E = Math.sqrt((this.EARTH_RADIUS_SQR - this.POLAR_RADIUS_SQR) / this.EARTH_RADIUS_SQR);  // 6.69437999014×10^−3
+        private E_PRIME = Math.sqrt((this.EARTH_RADIUS_SQR - this.POLAR_RADIUS_SQR) / this.POLAR_RADIUS_SQR); // 6.73949674228×10^−3
 
         // prime vertical radius of curvature => https://en.wikipedia.org/wiki/Earth_radius#Prime_vertical
-        function CalcN(latitude) {
-            return EARTH_RADIUS / (Math.sqrt(1 - E * E * Math.pow(Math.sin(latitude), 2)));
+        CalcN(latitude) {
+            return this.EARTH_RADIUS / (Math.sqrt(1 - this.E * this.E * Math.pow(Math.sin(latitude), 2)));
         }
 
-        function degreesToRad(degrees: number) {
+        degreesToRad(degrees: number) {
             return degrees * (Math.PI / 180);
         };
 
-        function RadTodegrees(rad: number) {
+        RadTodegrees(rad: number) {
             return rad * (180 / Math.PI);;
         };
 
-        function GeodeticToECEF(latitude, longitude, height) {
-            latitude = degreesToRad(latitude);
-            longitude = degreesToRad(longitude);
+        GeodeticToECEF(latitude, longitude, height) {
+            latitude = this.degreesToRad(latitude);
+            longitude = this.degreesToRad(longitude);
             // calculate N(phi)
-            const N = CalcN(latitude);
+            const N = this.CalcN(latitude);
 
             //Now calculate the Cartesian coordinates
             const X = (N + height) * Math.cos(latitude) * Math.cos(longitude);
             const Y = (N + height) * Math.cos(latitude) * Math.sin(longitude);
-            const Z = ((POLAR_RADIUS_SQR / EARTH_RADIUS_SQR) * N + height) * Math.sin(latitude);
+            const Z = ((this.POLAR_RADIUS_SQR / this.EARTH_RADIUS_SQR) * N + height) * Math.sin(latitude);
 
             return [X, Y, Z];
         }
 
-        function ECEFToGeodetic(X, Y, Z) {
+        ECEFToGeodetic(X, Y, Z) {
             //Auxiliary values first
             const p = Math.sqrt(X * X + Y * Y);
-            const theta = Math.atan((Z * EARTH_RADIUS) / (p * POLAR_RADIUS));
+            const theta = Math.atan((Z * this.EARTH_RADIUS) / (p * this.POLAR_RADIUS));
 
             const sintheta = Math.sin(theta);
             const costheta = Math.cos(theta);
 
-            const num = Z + E_PRIME * E_PRIME * POLAR_RADIUS * sintheta * sintheta * sintheta;
-            const denom = p - E * E * EARTH_RADIUS * costheta * costheta * costheta;
+            const num = Z + this.E_PRIME * this.E_PRIME * this.POLAR_RADIUS * sintheta * sintheta * sintheta;
+            const denom = p - this.E * this.E * this.EARTH_RADIUS * costheta * costheta * costheta;
 
             //Now calculate LLA
             const latitude = Math.atan(num / denom);
             // let longitude = Math.atan(Y/X);
             const longitude = Math.atan2(Y, X);
 
-            const N = CalcN(latitude);
+            const N = this.CalcN(latitude);
             const altitude = (p / Math.cos(latitude)) - N;
 
             // if (X < 0 && Y < 0) {
@@ -75,11 +73,10 @@ export class LocationService {
             //     longitude = longitude + Math.PI;
             // }
 
-            return [RadTodegrees(latitude), RadTodegrees(longitude), altitude];
+            return [this.RadTodegrees(latitude), this.RadTodegrees(longitude), altitude];
         }
 
-
-        // Helper function to calculate the distance in meter between two coordinates
+            // Helper function to calculate the distance in meter between two coordinates
         // function coordinateDistanceMeter(latA: number, lonA: number, latB: number, lonB: number) {
 
 
@@ -93,17 +90,19 @@ export class LocationService {
         //     const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
         //     return EARTH_RAD * c;
         // }
-        const BASE_FACTOR = -61.1;
-        const ENV_FACTOR = 2;
+        private BASE_FACTOR = -61.1;
+        private ENV_FACTOR = 2;
 
-        function rssiToMeters(rssi: number) {
-            if (rssi < BASE_FACTOR)
-                return Math.pow(10, ((BASE_FACTOR - rssi) / (10 * ENV_FACTOR)))
+        rssiToMeters(rssi: number) {
+            if (rssi < this.BASE_FACTOR)
+                return Math.pow(10, ((this.BASE_FACTOR - rssi) / (10 * this.ENV_FACTOR)))
             else
                 return 1;
             // return Math.pow(10,((BASE_FACTOR) / 10 * ENV_FACTOR))
         }
 
+
+    calculateLocation(gatewayData: UplinkRXInfo[], thingsBoardDeviceToken: string) {
         // -25.82198978570606, 28.33657264709473
         // -25.822120162959596, 28.33628833293915
         // console.log('RSSI: '+gatewayData[0].getRssi()+'  '+gatewayData[0].getTime().toDate().toISOString());
@@ -122,12 +121,12 @@ export class LocationService {
         })
 
         // calculate distance from RSSI
-        const len1Meter = rssiToMeters(gatewayData[0].getRssi());
-        const len1Degrees = len1Meter / EARTH_RADIUS;
-        const len2Meter = rssiToMeters(gatewayData[1].getRssi());
-        const len2Degrees = len2Meter / EARTH_RADIUS;
-        const len3Meter = rssiToMeters(gatewayData[2].getRssi());
-        const len3Degrees = len3Meter / EARTH_RADIUS;
+        const len1Meter = this.rssiToMeters(gatewayData[0].getRssi());
+        const len1Degrees = len1Meter / this.EARTH_RADIUS;
+        const len2Meter = this.rssiToMeters(gatewayData[1].getRssi());
+        const len2Degrees = len2Meter / this.EARTH_RADIUS;
+        const len3Meter = this.rssiToMeters(gatewayData[2].getRssi());
+        const len3Degrees = len3Meter / this.EARTH_RADIUS;
 
         console.log('dist1 ' + len1Meter);
         console.log('dist2 ' + len2Meter);
@@ -175,9 +174,9 @@ export class LocationService {
         // ====================
         // We need to do conversion from our current geodetic form to ECEF (latlong to euclidean)
         console.log("Height: " + gw1Height);
-        const gw1ECEF = GeodeticToECEF(gw1Lat, gw1Long, len1Meter);
-        const gw2ECEF = GeodeticToECEF(gw2Lat, gw2Long, len2Meter);
-        const gw3ECEF = GeodeticToECEF(gw3Lat, gw3Long, len3Meter);
+        const gw1ECEF = this.GeodeticToECEF(gw1Lat, gw1Long, len1Meter);
+        const gw2ECEF = this.GeodeticToECEF(gw2Lat, gw2Long, len2Meter);
+        const gw3ECEF = this.GeodeticToECEF(gw3Lat, gw3Long, len3Meter);
 
         const input = [
             //      X     Y     R
@@ -190,18 +189,18 @@ export class LocationService {
         // console.log(output);
         const avgHeight = (gw1ECEF[2] + gw2ECEF[2] + gw3ECEF[2]) / 3;
 
-        const location_Geodetic = ECEFToGeodetic(output[0], output[1], avgHeight);
+        const location_Geodetic = this.ECEFToGeodetic(output[0], output[1], avgHeight);
         // console.log(location_Geodetic);
         const location = { Latitude: location_Geodetic[0], Longitude: location_Geodetic[1] };
         this.tbClient.v1SendTelemetry(thingsBoardDeviceToken, location);
 
     }
 
-    MetersToRSSI(meters: number, BASE_FACTOR: number, ENV_FACTOR: number): number {
+    MetersToRSSI(meters: number): number {
         if (meters == 1)
-            return BASE_FACTOR
+            return this.BASE_FACTOR
         else
-            return BASE_FACTOR - 10 * ENV_FACTOR * Math.log10(meters)
+            return this.BASE_FACTOR - 10 * this.ENV_FACTOR * Math.log10(meters)
     }
 
     /*
