f7ff9999f585629ddb494f2b985c8d544f577787
LiamBurgesss <u18015001@tuks.co.za>
2022-08-13 10:43:03 +0200

    ✅ (tests): map api tests

--- master/libs/api/map-endpoint/src/lib/api-map-endpoint.service.ts
+++ master/libs/api/map-endpoint/src/lib/api-map-endpoint.service.ts
@@ -7,69 +7,6 @@ export class ApiMapEndpointService {
 
     constructor(private thingsboardClient: ThingsboardThingsboardClientService) { }
 
-    LatestProcess(content: MapApiLatest): MapApiLatestResponse {
-        /* Validate Token and ReserveID */
-        if (content.reserveID == undefined)
-            return {
-                code: 401,
-                status: 'failure',
-                explanation: "ReserveID missing"
-            }
-
-        if (content.token == undefined)
-            return {
-                code: 401,
-                status: 'failure',
-                explanation: "Token missing"
-            }
-        /* Get Device details and locations */
-        /***********************************/
-        /* Build response */
-        return {
-            code: 200,
-            status: "success",
-            explanation: "",
-            // data: [
-            //     {
-            //         deviceID: "sens-11",
-            //         deviceName: "Lion (harry)",
-            //         type: "sensor",
-            //         locationData: {
-            //             timeStamp: Date.now(),
-            //             location: {
-            //                 latitude: '-25.755375',
-            //                 longitude: '28.232314'
-            //             }
-            //         }
-            //     },
-            //     {
-            //         deviceID: "sens-12",
-            //         deviceName: "Zebra (larry)",
-            //         type: "sensor",
-            //         locationData: {
-            //             timeStamp: Date.now(),
-            //             location: {
-            //                 latitude: '-25.755704',
-            //                 longitude: '28.233245'
-            //             }
-            //         }
-            //     },
-            //     {
-            //         deviceID: "sens-13",
-            //         deviceName: "Giraffe (parry)",
-            //         type: "sensor",
-            //         locationData: {
-            //             timeStamp: Date.now(),
-            //             location: {
-            //                 latitude: '-25.756632',
-            //                 longitude: '28.233760'
-            //             }
-            //         }
-            //     }
-            // ]
-        }
-    }
-
     async ReserveProcess(content: MapApiReserve): Promise<MapApiReserveResponse> {
         //console.log(content)
         /* Validate Token and ReserveID */
@@ -292,4 +229,67 @@ export class ApiMapEndpointService {
             furtherExplain: furtherExplain
         }
     }
+
+       /* LatestProcess(content: MapApiLatest): MapApiLatestResponse {
+        /* Validate Token and ReserveID */
+       /* if (content.reserveID == undefined)
+            return {
+                code: 401,
+                status: 'failure',
+                explanation: "ReserveID missing"
+            }
+
+        if (content.token == undefined)
+            return {
+                code: 401,
+                status: 'failure',
+                explanation: "Token missing"
+            }
+        /* Get Device details and locations */
+        /***********************************/
+        /* Build response */
+       /* return {
+            code: 200,
+            status: "success",
+            explanation: "",
+            // data: [
+            //     {
+            //         deviceID: "sens-11",
+            //         deviceName: "Lion (harry)",
+            //         type: "sensor",
+            //         locationData: {
+            //             timeStamp: Date.now(),
+            //             location: {
+            //                 latitude: '-25.755375',
+            //                 longitude: '28.232314'
+            //             }
+            //         }
+            //     },
+            //     {
+            //         deviceID: "sens-12",
+            //         deviceName: "Zebra (larry)",
+            //         type: "sensor",
+            //         locationData: {
+            //             timeStamp: Date.now(),
+            //             location: {
+            //                 latitude: '-25.755704',
+            //                 longitude: '28.233245'
+            //             }
+            //         }
+            //     },
+            //     {
+            //         deviceID: "sens-13",
+            //         deviceName: "Giraffe (parry)",
+            //         type: "sensor",
+            //         locationData: {
+            //             timeStamp: Date.now(),
+            //             location: {
+            //                 latitude: '-25.756632',
+            //                 longitude: '28.233760'
+            //             }
+            //         }
+            //     }
+            // ]
+        }
+    }*/
 }
