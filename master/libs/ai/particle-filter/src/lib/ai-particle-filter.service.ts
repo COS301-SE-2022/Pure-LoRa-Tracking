3ecdb3a7f0e68278e137b9343cd55f426553fbb3
LiamBurgesss <u18015001@tuks.co.za>
2022-09-09 21:47:09 +0200

    ✨ (ai): change constructor

--- master/libs/ai/particle-filter/src/lib/ai-particle-filter.service.ts
+++ master/libs/ai/particle-filter/src/lib/ai-particle-filter.service.ts
@@ -4,12 +4,21 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AiParticleFilterService {
 
-    constructor(private locationComputations: LocationService) { }
+    constructor(private locationComputations: LocationService, initalParamters : {
+        reservePolygon: {latitude:number, longitude:number}[],
+        gateways : {latitude:number, longitude:number}[]
+    }) {
+        this.gatewayLocations = initalParamters.gateways;
+        this.reservePolygon = initalParamters.reservePolygon;
+     }
 
-    private particleSampleSetLatLong: { latitude: number, longitude: number }[];
-    private gatewayLocations: { latitude: number, longtitude: number }[];
+    private particles: { latitude: number, longitude: number }[];
+    private gatewayLocations: { latitude: number, longitude: number }[];
     private weights: [number];
-    private normMean = 0;
-    private particleSampleSet: {x:number, y:number, z:number}[];
+    private reservePolygon: { latitude: number, longitude: number }[];
+
+    generatePolygonSamples(howMany:number) {
+        return null;
+    }
 
 }
