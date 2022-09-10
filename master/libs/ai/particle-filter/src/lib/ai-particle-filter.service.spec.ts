452e9b537134cdf3e48d16fee9a4902d8da196a9
LiamBurgesss <u18015001@tuks.co.za>
2022-09-10 10:34:44 +0200

    ✨ (pf): random points in poly

--- master/libs/ai/particle-filter/src/lib/ai-particle-filter.service.spec.ts
+++ master/libs/ai/particle-filter/src/lib/ai-particle-filter.service.spec.ts
@@ -1,3 +1,6 @@
+import { LocationModule } from '@lora/location';
+import { ThingsboardThingsboardClientModule } from '@lora/thingsboard-client';
+import { getModelToken } from '@nestjs/mongoose';
 import { Test } from '@nestjs/testing';
 import { AiParticleFilterService } from './ai-particle-filter.service';
 
@@ -6,13 +9,70 @@ describe('AiParticleFilterService', () => {
 
   beforeEach(async () => {
     const module = await Test.createTestingModule({
+      imports:[LocationModule, ThingsboardThingsboardClientModule],
       providers: [AiParticleFilterService],
     }).compile();
 
     service = module.get(AiParticleFilterService);
+
+    service.addInitialParamters(initialParameters)
+
   });
 
   it('should be defined', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should generate a random point', () => {
+    service.generatePolygonSamples(1000)
+  });
 });
+
+
+const initialParameters = {
+  reservePolygon : [
+    {
+      longitude:28.25060248374939,
+      latitude:-25.749709741477048
+    },
+    {
+      longitude:28.25075268745422,
+      latitude:-25.7486177646462
+    },
+    {
+      longitude:28.249357938766476,
+      latitude:-25.748182903821597
+    
+    },
+    {
+      longitude:28.248413801193237,
+      latitude:-25.74940051011898
+    },
+    {
+      longitude:28.25060248374939,
+      latitude:-25.749709741477048
+    }
+  ],
+  gateways : [
+    {
+      longitude:28.250527381896973,
+      latitude:-25.748791708530312
+    },
+    {
+      longitude:28.249475955963135,
+      latitude:-25.748482474782456
+    },
+    {
+      longitude:28.249014616012573,
+      latitude:-25.74928454815217
+    },
+    {
+      longitude:28.250334262847897,
+      latitude:-25.749487481519804
+    },
+    {
+      longitude:28.250527381896973,
+      latitude:-25.748791708530312
+    }
+  ]
+}
--- master/libs/ai/particle-filter/src/lib/ai-particle-filter.service.ts
+++ master/libs/ai/particle-filter/src/lib/ai-particle-filter.service.ts
@@ -1,24 +1,59 @@
 import { LocationService } from '@lora/location';
 import { Injectable } from '@nestjs/common';
-
+import randomPositionInPolygon = require('random-position-in-polygon');
 @Injectable()
 export class AiParticleFilterService {
 
-    constructor(private locationComputations: LocationService, initalParamters : {
-        reservePolygon: {latitude:number, longitude:number}[],
-        gateways : {latitude:number, longitude:number}[]
-    }) {
-        this.gatewayLocations = initalParamters.gateways;
-        this.reservePolygon = initalParamters.reservePolygon;
+    /* considerations: 
+        env file for config parameters? 
+    */
+
+    constructor(private locationComputations: LocationService) {
+        this.reservePolygon = new Array<[number, number]>();
+        this.gatewayLocations = new Array<[number, number]>();
+        this.particles = new Array<[number, number]>();
      }
 
-    private particles: { latitude: number, longitude: number }[];
-    private gatewayLocations: { latitude: number, longitude: number }[];
-    private weights: [number];
-    private reservePolygon: { latitude: number, longitude: number }[];
-
-    generatePolygonSamples(howMany:number) {
-        return null;
+    addInitialParamters(initialParameters: {
+        reservePolygon: { latitude: number, longitude: number }[],
+        gateways: { latitude: number, longitude: number }[]
+    }) {
+        initialParameters.gateways.forEach((gateway) => {
+            this.gatewayLocations.push([gateway.longitude, gateway.latitude])
+        })
+        initialParameters.reservePolygon.forEach((location) => {
+            this.reservePolygon.push([location.longitude, location.latitude]);
+        })
     }
 
+    private particles: [number, number][];
+    private gatewayLocations: [number, number][];
+    private weights: [number];
+    private reservePolygon: [number, number][];
+
+    generatePolygonSamples(howMany: number) {
+        const polygon = {
+            type: 'Feature',
+            properties: {},
+            geometry: {
+                type: "Polygon",
+                "coordinates": [
+                    this.reservePolygon
+                ]
+            }
+        }
+        for (let i = 0; i < howMany; i++) {
+            this.particles.push(randomPositionInPolygon(polygon))
+        }
+    }
+
+    
+    /* TODO add color parameter */
+    printGeoJSONPoints(points:[number, number][]) {
+        let sline = ''
+        points.forEach((point:[number, number]) => {
+            sline += ('{"type": "Feature","properties": {},"geometry": {"type": "Point","coordinates": ['+point[0].toString()+','+point[1].toString()+']}},')
+        })
+        console.log(sline)
+    }
 }
