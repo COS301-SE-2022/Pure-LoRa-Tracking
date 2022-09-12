b90a5d66c1ecdb302651553ba7d41647f5af5713
LiamBurgesss <u18015001@tuks.co.za>
2022-09-12 09:35:36 +0200

    ✅ (tests): fix app test

--- master/apps/services/ai-processing/src/app/app.controller.spec.ts
+++ master/apps/services/ai-processing/src/app/app.controller.spec.ts
@@ -1,4 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { ProcessingApiProcessingBusModule } from '@processing/bus';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -8,8 +9,13 @@ describe('AppController', () => {
 
   beforeAll(async () => {
     app = await Test.createTestingModule({
+      imports:[ProcessingApiProcessingBusModule],
       controllers: [AppController],
       providers: [AppService],
     }).compile();
   });
+
+  it('works', ()=> {
+    expect(app).toBeTruthy
+  })
 });
