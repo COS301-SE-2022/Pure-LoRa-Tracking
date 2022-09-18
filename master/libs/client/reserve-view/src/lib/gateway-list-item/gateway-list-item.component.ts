da743b4a123e4ee053a1c5493fb15be400fd4acf
Brandon Kruger <u18019499@tuks.co.za>
2022-09-18 09:14:50 +0200

    :art: (UI): Item functionality

--- master/libs/client/reserve-view/src/lib/gateway-list-item/gateway-list-item.component.ts
+++ master/libs/client/reserve-view/src/lib/gateway-list-item/gateway-list-item.component.ts
@@ -1,12 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Gateway } from '@master/shared-interfaces';
 
 @Component({
   selector: 'master-gateway-list-item',
   templateUrl: './gateway-list-item.component.html',
   styleUrls: ['./gateway-list-item.component.scss'],
 })
-export class GatewayListItemComponent implements OnInit {
-  constructor() {}
+export class GatewayListItemComponent  {
+  @Input() gateway: Gateway;
+  @Output() openGatewayView = new EventEmitter<{"id":string,"name":string}>();
+  @Output() locateGateway = new EventEmitter<string>();
+  @Output() gatewayExpanded = new EventEmitter();
 
-  ngOnInit(): void {}
+  constructor() {
+    this.gateway = {
+      eui: "",
+      id: "",
+      name: "",
+      location:{
+        latitude: 0,
+        longitude: 0
+  
+      }
+    }
+  }
+
+  openGateway(): void {
+    this.openGatewayView.emit({
+      id: this.gateway.id,
+      name: this.gateway.name,
+    })
+  }
+  
+  locateOnMap(): void {
+    this.locateGateway.emit(this.gateway.id);
+  }
+
+  itemExpand(): void{
+    this.gatewayExpanded.emit();
+  }
+
 }
