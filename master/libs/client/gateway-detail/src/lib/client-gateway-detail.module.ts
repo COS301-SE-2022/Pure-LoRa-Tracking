7e3c427f51225b0127c323dd6a532875368ed877
Brandon Kruger <u18019499@tuks.co.za>
2022-09-19 18:46:40 +0200

    :art: (UI): Gateway detail main component

--- master/libs/client/gateway-detail/src/lib/client-gateway-detail.module.ts
+++ master/libs/client/gateway-detail/src/lib/client-gateway-detail.module.ts
@@ -2,11 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Route } from '@angular/router';
 import { GatewayViewDetailComponent } from './gateway-view-detail/gateway-view-detail.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTabsModule } from '@angular/material/tabs';
 
 export const clientGatewayDetailRoutes: Route[] = [];
 
 @NgModule({
-  imports: [CommonModule, RouterModule],
+  imports: [
+    CommonModule, 
+    RouterModule,
+    MatCardModule,
+    MatDividerModule,
+    MatButtonModule,
+    MatIconModule,
+    MatTabsModule
+  ],
   declarations: [GatewayViewDetailComponent],
   exports: [GatewayViewDetailComponent],
 })
--- master/libs/client/gateway-detail/src/lib/gateway-view-detail/gateway-view-detail.component.ts
+++ master/libs/client/gateway-detail/src/lib/gateway-view-detail/gateway-view-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Gateway } from '@master/shared-interfaces';
 
 @Component({
   selector: 'master-gateway-view-detail',
@@ -6,7 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gateway-view-detail.component.scss'],
 })
 export class GatewayViewDetailComponent implements OnInit {
-  constructor() {}
+  @Input() gatewayInfo: Gateway;
+
+  constructor() {
+    this.gatewayInfo = {
+      eui: "",
+      id: "",
+      name: "",
+      location: {
+        latitude: 0,
+        longitude: 0
+      }
+    }
+  }
 
   ngOnInit(): void {}
 }
