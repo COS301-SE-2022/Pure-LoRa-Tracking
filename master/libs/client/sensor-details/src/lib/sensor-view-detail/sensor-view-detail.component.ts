25758318b0849ef84b5d43f1f3e0d87af7dd3561
MildogMiller <37893674+MildogMiller@users.noreply.github.com>
2022-09-20 15:38:32 +0200

    ✨ communication data logic

--- master/libs/client/sensor-details/src/lib/sensor-view-detail/sensor-view-detail.component.ts
+++ master/libs/client/sensor-details/src/lib/sensor-view-detail/sensor-view-detail.component.ts
@@ -5,7 +5,7 @@ import { TokenManagerService } from '@master/client/user-storage-controller';
 import { DialogConfirmationComponent } from '@master/client/shared-ui/components-ui';
 import { DeviceNotifierService } from '@master/client/shared-services';
 import { HttpClient } from '@angular/common/http';
-import { string } from '@tensorflow/tfjs-node';
+import { data, string } from '@tensorflow/tfjs-node';
 
 @Component({
   selector: 'master-sensor-view-detail',
@@ -36,15 +36,21 @@ export class SensorViewDetailComponent implements OnInit {
     this.http.post("api/device/admin/sensordata", {
       "deviceEUI": this.sensorInfo.id
     }).subscribe((val: any) => {
-      let temp=[];
-      console.log("dATA received ", val);
+      let temp = [];
+      console.log("data received ", val);
+      const rawdata = val.data;
+      let objnames=Object.keys(rawdata);
+      let joined:any={};
       if (val.status == 200 && val.explanation == "ok") {
-        for (let key in val.data) {
+        for (let key in rawdata) {
           if (key.startsWith("data_")) {
             temp.push(
               {
                 graphname: key.substring(5),
-                data: val.data[key]?.map((curr: any) => {
+                data: rawdata[key]?.map((curr: any) => {
+                  //use this opportunity to grab the comms data
+                  joined[curr.ts.toString()]={...joined[curr.ts.toString()], [key]:key.substring(5)};
+                  //return for the graph data
                   return {
                     name: new Date(curr.ts),
                     value: curr.value
@@ -53,19 +59,26 @@ export class SensorViewDetailComponent implements OnInit {
               }
             )
           }
+          else{
+            //add to array if not in data_
+            rawdata[key]?.forEach((curr: any) =>{
+              joined[curr.ts.toString()]={...joined[curr.ts.toString()], [key]:key};
+            })
+          }
         }
       }
-      this.rawGraphdata=temp;//setting with a temp to trigger change detection
+      this.rawGraphdata = temp;//setting with a temp to trigger change detection
     })
 
 
     // const headersReq = {
     //   'Content-Type': 'application/json',
-    //   Authorization: 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJyZXNlcnZlYWRtaW5AcmVzZXJ2ZS5jb20iLCJ1c2VySWQiOiJmNzRjYmYzMC0wODNlLTExZWQtYmM2ZS1hNTAwNjJmNmNkYmEiLCJzY29wZXMiOlsiVEVOQU5UX0FETUlOIl0sImlzcyI6InRoaW5nc2JvYXJkLmlvIiwiaWF0IjoxNjYzNjY0Njc0LCJleHAiOjE2NjM2NzM2NzQsImZpcnN0TmFtZSI6InJlc2VydmUiLCJsYXN0TmFtZSI6ImFkbWluIiwiZW5hYmxlZCI6dHJ1ZSwiaXNQdWJsaWMiOmZhbHNlLCJ0ZW5hbnRJZCI6ImVjODY0MzUwLTA4M2UtMTFlZC1iYzZlLWE1MDA2MmY2Y2RiYSIsImN1c3RvbWVySWQiOiIxMzgxNDAwMC0xZGQyLTExYjItODA4MC04MDgwODA4MDgwODAifQ.uL_zAJFo5EBJWEKCUIgf6gPBpq3m_bNSZJVYjYmXlIzTVM1JWdPOt9KX_MlFRmwH43u_I-N21pICcJrFbA7jig',
+    //   Authorization: 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJyZXNlcnZlYWRtaW5AcmVzZXJ2ZS5jb20iLCJ1c2VySWQiOiJmNzRjYmYzMC0wODNlLTExZWQtYmM2ZS1hNTAwNjJmNmNkYmEiLCJzY29wZXMiOlsiVEVOQU5UX0FETUlOIl0sImlzcyI6InRoaW5nc2JvYXJkLmlvIiwiaWF0IjoxNjYzNjczNjg2LCJleHAiOjE2NjM2ODI2ODYsImZpcnN0TmFtZSI6InJlc2VydmUiLCJsYXN0TmFtZSI6ImFkbWluIiwiZW5hYmxlZCI6dHJ1ZSwiaXNQdWJsaWMiOmZhbHNlLCJ0ZW5hbnRJZCI6ImVjODY0MzUwLTA4M2UtMTFlZC1iYzZlLWE1MDA2MmY2Y2RiYSIsImN1c3RvbWVySWQiOiIxMzgxNDAwMC0xZGQyLTExYjItODA4MC04MDgwODA4MDgwODAifQ.b4_l69ULFEcDTkpQlDzadPqzdfGw9CxVsLmsvm2r7U_mrIQaHFKiSW73MNGKzqRy_L4X7bxogRROQidv8SgqWg',
     // };
     // for(let i=10; i>1;i--){
     //   this.http.post("http://localhost:9090/api/plugins/telemetry/DEVICE/47af66c0-0840-11ed-bc6e-a50062f6cdba/timeseries/any",{
-    //       "data_graph":Math.floor(Math.random()*900)+100
+    //       "data_graph":Math.floor(Math.random()*900)+100,
+    //       "data_graph2":Math.floor(Math.random()*900)+100
     //     },{headers:headersReq}).subscribe(val=>{
     //     console.log("val>> ",val);
     //   })
@@ -90,31 +103,6 @@ export class SensorViewDetailComponent implements OnInit {
     this.openView = false;
     this.viewChange.emit(this.openView);
   }
-
-  // deleteSensor():void{
-  //   const mydialog=this.dialogcontroller.open(DialogConfirmationComponent,{
-  //     data: {
-  //       title: 'Confirm Delete',
-  //       dialogMessage: 'Are you sure you want to delete Sensor: '+this.sensorInfo.id+'?  NOTE: THIS ACTION CANNOT BE REVERSED!',
-  //     },
-  //   })
-  //   mydialog.afterClosed().subscribe(val=>{
-  //     if(val){
-  //       this.apicaller.removeDevice(this.sensorInfo.id,this.sensorInfo.name,false).then(curr=>{
-  //         //TODO change alert to proper pop up @brandon-c-k
-  //         if(curr.status==200&&curr.explanation=="ok"){
-  //           alert("Device deleted");
-  //           this.notifier.deleteSensor(this.sensorInfo.id)
-  //           this.closeSensor();
-  //         }
-  //         else {
-  //           console.log(curr)
-  //           alert("Something went wrong");
-  //         }
-  //       });
-  //     }
-  //   })
-  // }
 
   locateSensor(): void {
     this.notifier.locateSensor(this.sensorInfo.id);
