7b96e3a1e8766ef03dc675b12e986ef36359dd9e
t3ddy33 <u20493836@tuks.co.za>
2022-09-20 17:39:04 +0200

    🔨 (dev): Add heatMapTestParameters interface

--- master/apps/tests/ai/src/app/app.controller.ts
+++ master/apps/tests/ai/src/app/app.controller.ts
@@ -8,19 +8,31 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Post('pf')
-  async processPerimeter(@Body() content : testParamters) : Promise<{status:number, data:any}> {return await this.appService.processParticleFilterTest(content)};
+  async processPerimeter(
+    @Body() content: testParamters
+  ): Promise<{ status: number; data: any }> {
+    return await this.appService.processParticleFilterTest(content);
+  }
 }
 
 export interface PFprocessingParameters {
-  perimeter : number[],
-  gateways : number[],
-  reading : number[],
-  trainingIterations: number,
-  numberOfParticles: number,
-  name : string
+  perimeter: number[];
+  gateways: number[];
+  reading: number[];
+  trainingIterations: number;
+  numberOfParticles: number;
+  name: string;
 }
 
 export interface testParamters {
-  PFs : PFprocessingParameters[],
-  NNs : null
+  PFs: PFprocessingParameters[];
+  NNs: null;
+}
+
+export interface heatMapTestParameters {
+  devID: string;
+  reading: {
+    longitude: number;
+    latitude: number;
+  };
 }
