fbb03078d57296709c098b8d19a21c2cd4fb2aec
Brandon Kruger <u18019499@tuks.co.za>
2022-09-22 16:39:32 +0200

    :art: (UI): Status page component

--- master/libs/client/manage-view/src/lib/status-view/src/lib/client-manage-view-src-lib-status-view.module.ts
+++ master/libs/client/manage-view/src/lib/status-view/src/lib/client-manage-view-src-lib-status-view.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Route } from '@angular/router';
+import { StatusPageComponent } from './status-page/status-page.component';
 
 export const clientManageViewSrcLibStatusViewRoutes: Route[] = [];
 
 @NgModule({
   imports: [CommonModule, RouterModule],
+  declarations: [StatusPageComponent],
+  exports: [StatusPageComponent],
 })
 export class ClientManageViewSrcLibStatusViewModule {}
---
+++ master/libs/client/manage-view/src/lib/status-view/src/lib/status-page/status-page.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'master-status-page',
+  templateUrl: './status-page.component.html',
+  styleUrls: ['./status-page.component.scss'],
+})
+export class StatusPageComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
