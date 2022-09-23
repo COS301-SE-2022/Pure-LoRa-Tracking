32b0083337a61820d3ac9e3876e64cac06a38ea1
MildogMiller <37893674+MildogMiller@users.noreply.github.com>
2022-09-23 17:19:09 +0200

    🐛 Fixed import issue

--- master/libs/client/users-view/src/lib/authentication-view/authentication-view.component.spec.ts
+++ master/libs/client/users-view/src/lib/authentication-view/authentication-view.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AuthenticationViewComponent } from './authentication-view.component';
 
 describe('AuthenticationViewComponent', () => {
@@ -9,6 +9,7 @@ describe('AuthenticationViewComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [AuthenticationViewComponent],
+      imports: [ReactiveFormsModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(AuthenticationViewComponent);
