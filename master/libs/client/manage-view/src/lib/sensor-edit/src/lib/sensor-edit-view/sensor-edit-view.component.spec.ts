88fe201c672760987e3aab1d0ede972258251664
Brandon Kruger <u18019499@tuks.co.za>
2022-09-24 20:19:38 +0200

    :bug: (UI): Copy Testing

--- master/libs/client/manage-view/src/lib/sensor-edit/src/lib/sensor-edit-view/sensor-edit-view.component.spec.ts
+++ master/libs/client/manage-view/src/lib/sensor-edit/src/lib/sensor-edit-view/sensor-edit-view.component.spec.ts
@@ -4,8 +4,11 @@ import { SensorEditViewComponent } from './sensor-edit-view.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import {HttpClientTestingModule} from "@angular/common/http/testing"
 import { RouterTestingModule } from '@angular/router/testing';
-import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatSnackBarModule, MatSnackBar} from '@angular/material/snack-bar';
 import { MatDialogModule } from '@angular/material/dialog';
+import { httpMock, snackbarTesting } from '@master/shared-interfaces';
+import { HttpClient } from '@angular/common/http';
+import { of } from 'rxjs';
 
 describe('SensorEditViewComponent', () => {
   let component: SensorEditViewComponent;
@@ -15,6 +18,10 @@ describe('SensorEditViewComponent', () => {
     await TestBed.configureTestingModule({
       imports:[ReactiveFormsModule, HttpClientTestingModule, RouterTestingModule,MatSnackBarModule,MatDialogModule],
       declarations: [SensorEditViewComponent],
+      providers:[
+        { provide: HttpClient, useValue: httpMock },
+        { provide: MatSnackBar, useValue: snackbarTesting },
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(SensorEditViewComponent);
@@ -25,4 +32,34 @@ describe('SensorEditViewComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe("Assign device",()=>{
+    it("Assign Device -> call -> snackbar -> ngOnInit",()=>{
+      jest.spyOn(httpMock,"post").mockImplementationOnce(()=>of({
+        status:true,
+        explanation:"call finished"
+      }));
+      jest.spyOn(snackbarTesting,"openFromComponent").mockImplementation();
+      jest.spyOn(component,"ngOnInit").mockImplementation();
+      component.assign("test","test");
+      expect(httpMock.post).toBeCalled();
+      expect(snackbarTesting.openFromComponent).toBeCalled();
+      expect(component.ngOnInit).toBeCalled();
+    })
+  })
+
+  describe("Unassign device",()=>{
+    it("Unassign Device -> call -> snackbar -> ngOnInit",()=>{
+      jest.spyOn(httpMock,"post").mockImplementationOnce(()=>of({
+        status:true,
+        explanation:"call finished"
+      }));
+      jest.spyOn(snackbarTesting,"openFromComponent").mockImplementation();
+      jest.spyOn(component,"ngOnInit").mockImplementation();
+      component.unassign("test");
+      expect(httpMock.post).toBeCalled();
+      expect(snackbarTesting.openFromComponent).toBeCalled();
+      expect(component.ngOnInit).toBeCalled();
+    })
+  })
 });
