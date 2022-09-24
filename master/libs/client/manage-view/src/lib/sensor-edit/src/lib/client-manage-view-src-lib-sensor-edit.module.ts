18eecd64d0f30ba94375ee6951bbc00bd6860edf
Brandon Kruger <u18019499@tuks.co.za>
2022-09-24 07:59:39 +0200

    :art: (UI): Basic input

--- master/libs/client/manage-view/src/lib/sensor-edit/src/lib/client-manage-view-src-lib-sensor-edit.module.ts
+++ master/libs/client/manage-view/src/lib/sensor-edit/src/lib/client-manage-view-src-lib-sensor-edit.module.ts
@@ -3,6 +3,9 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Route } from '@angular/router';
 import { SensorEditViewComponent } from './sensor-edit-view/sensor-edit-view.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 export const clientManageViewSrcLibSensorEditRoutes: Route[] = [
   {path:'', component:SensorEditViewComponent}
@@ -10,7 +13,7 @@ export const clientManageViewSrcLibSensorEditRoutes: Route[] = [
 
 @NgModule({
   imports: [CommonModule, RouterModule, RouterModule.forChild(clientManageViewSrcLibSensorEditRoutes),
-    ReactiveFormsModule,ReactiveFormsModule],
+    ReactiveFormsModule,FormsModule,MatFormFieldModule,MatInputModule, MatButtonModule],
   declarations: [SensorEditViewComponent],
   exports: [SensorEditViewComponent],
 })
--- master/libs/client/manage-view/src/lib/sensor-edit/src/lib/sensor-edit-view/sensor-edit-view.component.ts
+++ master/libs/client/manage-view/src/lib/sensor-edit/src/lib/sensor-edit-view/sensor-edit-view.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { UntypedFormControl, Validators, UntypedFormBuilder, UntypedFormGroup } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Validators, UntypedFormBuilder, UntypedFormGroup } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'master-sensor-edit-view',
@@ -10,14 +11,33 @@ import { Router } from '@angular/router';
 export class SensorEditViewComponent implements OnInit {
 
   sensorDetails = {
-    name: ""
+    name: "",
+    id:"",
+    eui:"",
   };
 
   editSensor: UntypedFormGroup = new UntypedFormGroup({});
 
-  constructor(private formBuilder: UntypedFormBuilder, private router:Router) {
+  constructor(private formBuilder: UntypedFormBuilder, private router:Router, private http:HttpClient, private activeRoute:ActivatedRoute) {
+    this.activeRoute.paramMap.subscribe( params => { 
+      this.sensorDetails.id = params.get('id') || "";
+      this.sensorDetails.eui = params.get('devEUI') || "";
+    })
+
+    this.editSensor = this.formBuilder.group({
+      name: ['',[Validators.required]],
+    });
 
   }
 
   ngOnInit(): void {}
+
+  saveSensor():void{
+    console.log("save");
+  }
+
+  cancelEdit():void{
+    this.router.navigate(['manage',{outlets:{managecontent:['manage-devices']}}]);  
+  }
+
 }
