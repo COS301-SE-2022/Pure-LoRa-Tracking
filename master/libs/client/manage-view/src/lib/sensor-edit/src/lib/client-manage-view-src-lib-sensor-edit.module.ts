f555b243dd76e6a75a31d175d6e6075763aa7af5
Brandon Kruger <u18019499@tuks.co.za>
2022-09-24 08:46:36 +0200

    :art: (UI): Sensor move assign

--- master/libs/client/manage-view/src/lib/sensor-edit/src/lib/client-manage-view-src-lib-sensor-edit.module.ts
+++ master/libs/client/manage-view/src/lib/sensor-edit/src/lib/client-manage-view-src-lib-sensor-edit.module.ts
@@ -3,17 +3,31 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Route } from '@angular/router';
 import { SensorEditViewComponent } from './sensor-edit-view/sensor-edit-view.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatDialogModule } from '@angular/material/dialog';
 
 export const clientManageViewSrcLibSensorEditRoutes: Route[] = [
   {path:'', component:SensorEditViewComponent}
 ];
 
 @NgModule({
-  imports: [CommonModule, RouterModule, RouterModule.forChild(clientManageViewSrcLibSensorEditRoutes),
-    ReactiveFormsModule,FormsModule,MatFormFieldModule,MatInputModule, MatButtonModule],
+  imports: [
+    CommonModule, 
+    RouterModule, 
+    ReactiveFormsModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule, 
+    MatButtonModule, 
+    MatSelectModule, 
+    MatSnackBarModule, 
+    MatDialogModule,
+    RouterModule.forChild(clientManageViewSrcLibSensorEditRoutes),
+  ],
   declarations: [SensorEditViewComponent],
   exports: [SensorEditViewComponent],
 })
--- master/libs/client/manage-view/src/lib/sensor-edit/src/lib/sensor-edit-view/sensor-edit-view.component.ts
+++ master/libs/client/manage-view/src/lib/sensor-edit/src/lib/sensor-edit-view/sensor-edit-view.component.ts
@@ -2,6 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { Validators, UntypedFormBuilder, UntypedFormGroup } from '@angular/forms';
 import { Router, ActivatedRoute } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
+import { SnackbarAlertComponent } from '@master/client/shared-ui/components-ui';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatDialog } from '@angular/material/dialog';
+
+interface DeviceInterface {
+  id: string;
+  name: string;
+}
 
 @Component({
   selector: 'master-sensor-edit-view',
@@ -14,11 +22,15 @@ export class SensorEditViewComponent implements OnInit {
     name: "",
     id:"",
     eui:"",
+    reserve:"",
+    status: true
   };
 
   editSensor: UntypedFormGroup = new UntypedFormGroup({});
 
-  constructor(private formBuilder: UntypedFormBuilder, private router:Router, private http:HttpClient, private activeRoute:ActivatedRoute) {
+  reserveList:DeviceInterface[]=[];
+
+  constructor(private formBuilder: UntypedFormBuilder, private router:Router, private http:HttpClient, private activeRoute:ActivatedRoute,private snackbar:MatSnackBar,private dialogcontroller:MatDialog) {
     this.activeRoute.paramMap.subscribe( params => { 
       this.sensorDetails.id = params.get('id') || "";
       this.sensorDetails.eui = params.get('devEUI') || "";
@@ -30,7 +42,16 @@ export class SensorEditViewComponent implements OnInit {
 
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.http.post("api/user/admin/groups",{}).subscribe((val:any)=>{
+      this.reserveList = val.data.data.map((curr:any)=>{
+        return {
+          name:curr.name,
+          id:curr.id.id
+        }
+      });
+    })
+  }
 
   saveSensor():void{
     console.log("save");
@@ -38,6 +59,31 @@ export class SensorEditViewComponent implements OnInit {
 
   cancelEdit():void{
     this.router.navigate(['manage',{outlets:{managecontent:['manage-devices']}}]);  
+  }
+
+  unassign(id:string):void{
+    this.http.post("api/device/admin/unassign",{
+      deviceID:id
+    }).subscribe((val:any)=>{
+      console.log(val);
+      if(val.status&&val.explanation=="call finished"){
+        this.snackbar.openFromComponent(SnackbarAlertComponent,{duration: 5000, panelClass: ['green-snackbar'], data: {message:"Device Unassigned", icon:"check_circle"}});
+        this.ngOnInit();
+      }
+    });
+
+  }
+  
+  assign(id:string,reserveid:string):void{
+    this.http.post("api/device/admin/assign",{
+      deviceID:id,
+      customerID:reserveid
+    }).subscribe((val:any)=>{
+      if(val.status&&val.explanation=="call finished"){
+        this.snackbar.openFromComponent(SnackbarAlertComponent,{duration: 5000, panelClass: ['green-snackbar'], data: {message:"Device Assigned", icon:"check_circle"}});
+        this.ngOnInit();
+      }
+    }); 
   }
 
 }
