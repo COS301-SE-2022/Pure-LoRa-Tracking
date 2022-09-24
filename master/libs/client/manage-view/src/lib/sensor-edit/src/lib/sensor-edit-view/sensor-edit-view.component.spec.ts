47c4a23523ecd25d1363f111db60d7df9e6fb52b
Brandon Kruger <u18019499@tuks.co.za>
2022-09-24 19:51:54 +0200

    :bug: (UI): Providers

--- master/libs/client/manage-view/src/lib/sensor-edit/src/lib/sensor-edit-view/sensor-edit-view.component.spec.ts
+++ master/libs/client/manage-view/src/lib/sensor-edit/src/lib/sensor-edit-view/sensor-edit-view.component.spec.ts
@@ -1,6 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SensorEditViewComponent } from './sensor-edit-view.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import {HttpClientTestingModule} from "@angular/common/http/testing"
+import { RouterTestingModule } from '@angular/router/testing';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import { MatDialogModule } from '@angular/material/dialog';
 
 describe('SensorEditViewComponent', () => {
   let component: SensorEditViewComponent;
@@ -8,6 +13,7 @@ describe('SensorEditViewComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports:[ReactiveFormsModule, HttpClientTestingModule, RouterTestingModule,MatSnackBarModule,MatDialogModule],
       declarations: [SensorEditViewComponent],
     }).compileComponents();
 
