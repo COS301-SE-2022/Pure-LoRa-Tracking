dbe3a66e06809d1686dbc4ac1541df36de0c71b0
Brandon Kruger <u18019499@tuks.co.za>
2022-09-28 00:37:43 +0200

    :art: (UI): Empty Message Component

---
+++ master/libs/client/shared-ui/components-ui/src/lib/display-message/display-message.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'master-display-message',
+  templateUrl: './display-message.component.html',
+  styleUrls: ['./display-message.component.scss'],
+})
+export class DisplayMessageComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
