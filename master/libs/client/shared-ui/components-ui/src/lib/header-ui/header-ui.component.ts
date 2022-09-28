a56e5d6c229d7eb3977150fa365243ebd99e1ecc
MildogMiller <37893674+MildogMiller@users.noreply.github.com>
2022-09-28 15:06:33 +0200

    🎨 Moved admin check

--- master/libs/client/shared-ui/components-ui/src/lib/header-ui/header-ui.component.ts
+++ master/libs/client/shared-ui/components-ui/src/lib/header-ui/header-ui.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { CookieService } from 'ngx-cookie-service';
-import jwt_decode from "jwt-decode";
 @Component({
   selector: 'master-header-nav',
   templateUrl: './header-ui.component.html',
@@ -9,19 +8,16 @@ import jwt_decode from "jwt-decode";
 })
 export class HeaderUiComponent implements OnInit{
   logoLoc = "assets/Logos/logo.svg";
-  admin=false;
   constructor(private cookieservice:CookieService, private router:Router){}
 
   ngOnInit() :void {
-    let decoded:any;
-    const cookie= this.cookieservice.get("PURELORA_TOKEN")
-    if(cookie!=undefined&&cookie!=null&&cookie!="") decoded=jwt_decode(cookie);
-    if(decoded?.scopes[0]=="TENANT_ADMIN") this.admin=true;
+    
   }
 
   logout() {
     console.log("logout called");
     this.cookieservice.deleteAll();
+    this.cookieservice.deleteAll("/manage");//delete cookies for manage
     this.router.navigate(['']);
   }
 }
