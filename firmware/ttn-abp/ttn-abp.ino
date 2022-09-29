84fc1cacab59bc1aa4c0c6d4f8b4b55ee69eb115
Isakvdw <u20468203@tuks.co.za>
2022-09-29 08:52:13 +0200

    ✨(hardware) encode battery voltage

--- firmware/ttn-abp/ttn-abp.ino
+++ firmware/ttn-abp/ttn-abp.ino
@@ -1,5 +1,3 @@
-#include <Time.h>
-
 //
 #include <lmic.h>
 #include <hal/hal.h>
@@ -11,7 +9,7 @@
 AXP20X_Class axp;
 U8G2_SSD1306_128X64_NONAME_1_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
 
-char* version = "ttn-abp v1.0.37";
+char* version = "ttn-abp v1.0.39";
 
 //
 // LoRaWAN NwkSKey, network session key
@@ -37,13 +35,13 @@ void os_getArtEui (u1_t* buf) { }
 void os_getDevEui (u1_t* buf) { }
 void os_getDevKey (u1_t* buf) { }
 
-static uint8_t mydata[] = "Hello, world!";
+//static uint8_t mydata[] = "Hello";
+static uint8_t mydata[4] = {0,0,0,0};
 static osjob_t sendjob;
 
 // Schedule TX every this many seconds (might become longer due to duty
 // cycle limitations).
 const unsigned TX_INTERVAL = 10;
-
 
 const lmic_pinmap lmic_pins = {
   .nss =  18,
@@ -51,6 +49,7 @@ const lmic_pinmap lmic_pins = {
   .rst =  23,
   .dio = {26, 33, 32},
 };
+
 void onEvent (ev_t ev) {
   Serial.print(os_getTime());
   Serial.print(": ");
@@ -135,6 +134,7 @@ void setup() {
   if(axp.begin(Wire, AXP192_SLAVE_ADDRESS) == AXP_FAIL) {
     Serial.println(F("error communicating with AXP192 power chip"));
   }
+  
   axp.adc1Enable(AXP202_VBUS_VOL_ADC1 |
                    AXP202_VBUS_CUR_ADC1 |
                    AXP202_BATT_CUR_ADC1 |
@@ -251,11 +251,11 @@ void loop() {
     if (!digitalRead(38)) {
       state = (state < 3? state+1:0);
     }
-    Serial.print("battery: ");
-    Serial.print(axp.getBattVoltage()/1000);
-    Serial.print(" V, ");
-    Serial.print(axp.getBattPercentage());
-    Serial.println(" %");
+//    Serial.print("battery: ");
+//    Serial.print(axp.getBattVoltage()/1000);
+//    Serial.print(" V, ");
+//    Serial.print(axp.getBattPercentage());
+//    Serial.println(" %");
     u8g2.firstPage();
     do {
       
@@ -270,11 +270,21 @@ void loop() {
 
       
       double batt = axp.getBattVoltage()/1000;
+      int test = axp.getBattVoltage();
+      mydata[0] = (test & 0xFF);
+      mydata[1] = (test>>8);
+//      mydata[1] = ((batt-mydata[0]) * 100);
+//      mydata[2] = ((batt-mydata[0]) * 100);
+      Serial.println(test);
+      Serial.print("Battery: ");
+      Serial.println(mydata[0]);
+      Serial.print("Battery[1]: ");
+      Serial.println(mydata[1]);
       u8g2.setFontDirection(1);
       if (axp.isChargeing()) {
         u8g2.setFont(u8g2_font_siji_t_6x10);
         u8g2.drawGlyph(30, 55, 0xe215);
-        Serial.println("Charging");
+//        Serial.println("Charging");
       } 
 
       u8g2.setFont(u8g2_font_battery19_tn);
@@ -296,10 +306,10 @@ void loop() {
       u8g2.setFontDirection(0);  
 
       u8g2.setFont(u8g2_font_ncenR12_tr);
-      Serial.print("Charging: ");
-      Serial.println(axp.getBattChargeCurrent());
-      Serial.print("Usage: ");
-      Serial.println(axp.getBattDischargeCurrent());
+//      Serial.print("Charging: ");
+//      Serial.println(axp.getBattChargeCurrent());
+//      Serial.print("Usage: ");
+//      Serial.println(axp.getBattDischargeCurrent());
       switch (state) {
         case 0:    
           sprintf(batt_v, "Battery %g V", axp.getBattVoltage()/1000); //,%d %% axp.getBattPercentage()
